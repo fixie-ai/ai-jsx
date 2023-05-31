ac0d96efc668a125f46473a5ee98fbab0102f4c7
Nick Heiner <nick.heiner+github@gmail.com>
2023-05-31 00:42:19 -0400

    Expand comments

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -14,13 +14,21 @@ export interface Document<Metadata extends Jsonifiable = Jsonifiable> {
 export type Chunk = <T extends string | Document>(text: T) => Promise<T[]>;
 
 /**
- * Load data from a source.
+ * Load Documents from a source.
  *
- * This method returns documents. However, depending on the size of your documents and your context window, you may
- * wish to chunk those docs further. To do that, use a `Chunk` method.
+ * Examples of when you'd use this:
+ *  * Load and parse a file into documents.
+ *  * Call a remote API
+ *  * Load data from a database
+ *  * Do a k-nearest-neighbors search in a vector store.
+ *
+ * Once you have the Documents, you may wish to do some combination of:
+ *  1. Inserting them directly into a prompt
+ *  2. Adding them to a vector store.
+ *  3. Chunking them into smaller documents (use a `Chunk` method).
  *
  * In LangChain, there are loaders and retrievers. In AI.JSX, there are only loaders. Retrievers are a special
- * of loaders.
+ * case of loaders.
  */
 export type Load = (...args: any[]) => Promise<Document[]>;
 
