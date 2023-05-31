098f68bb889cbd558a04dbbd7efe3a0a9d24e9bb
Nick Heiner <nick.heiner+github@gmail.com>
2023-05-31 00:23:32 -0400

    Tweaking interfaces + adding comments

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -6,11 +6,20 @@ export interface Document<Metadata extends Jsonifiable = Jsonifiable> {
 }
 
 /**
- * Split text into multiple documents.
+ * Chunk text into multiple documents.
+ *
+ * In LangChain, this is called "split". We use the phrase "chunk", because the cannonical term for the output
+ * of this process is "chunks".
  */
-export type Split = <T extends string | Document>(text: T) => Promise<T[]>;
+export type Chunk = <T extends string | Document>(text: T) => Promise<T[]>;
 
 /**
  * Load data from a source.
+ *
+ * This method returns documents. However, depending on the size of your documents and your context window, you may
+ * wish to chunk those docs further. To do that, use a `Chunk` method.
+ *
+ * In LangChain, there are loaders and retrievers. In AI.JSX, there are only loaders. Retrievers are a special
+ * of loaders.
  */
 export type Load = (...args: any[]) => Promise<Document[]>;
