3e853df993e924fd24037859c888d8d9d6be6004
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-01 13:47:46 -0400

    Now we can simplify, for the low low price of o(inspector components) ts-expect-errors

--- src/inspector/console.tsx
+++ src/inspector/console.tsx
@@ -2,57 +2,63 @@ import { LLMx } from '../lib/index.ts';
 import { useState, useEffect } from 'react';
 import reactUse from 'react-use';
 import { render, Text, useInput } from 'ink';
-import { debug, Element } from './sad-copy.ts'
+import { DebugTree } from '../lib/debug.tsx';
 
 const {useList} = reactUse;
 
-function DebugTree(props: { children: LLMx.Node }) {
-  return <></>;
-}
 
 function Inspector({componentToInspect}: {componentToInspect: any}) {
   const [steps, {push}] = useList([] as string[]);
-  const [frameIndex, setFrameIndex] = useState(0);
+  // const [frameIndex, setFrameIndex] = useState(0);
 
-  useEffect(() => {
-    // async function getAllFrames() {
-    //   while (true) {
-    //     yield debug(<DebugTree {...props}>{current}</DebugTree>);
+  // useEffect(() => {
+  //   async function getAllFrames() {
+  //     // while (true) {
+  //     //   yield debug(<DebugTree {...props}>{current}</DebugTree>);
     
-    //     let elementToRender: LLMx.Element<any> | null = null;
-    //     const shouldStop = (element: LLMx.Element<any>): boolean => {
-    //       if (elementToRender === null) {
-    //         elementToRender = element;
-    //       }
-    //       return element !== elementToRender;
-    //     };
+  //     //   let elementToRender: LLMx.Element<any> | null = null;
+  //     //   const shouldStop = (element: LLMx.Element<any>): boolean => {
+  //     //     if (elementToRender === null) {
+  //     //       elementToRender = element;
+  //     //     }
+  //     //     return element !== elementToRender;
+  //     //   };
     
-    //     // Use a closure to prevent the type from being incorrectly narrowed.
-    //     // https://github.com/microsoft/TypeScript/issues/9998#issuecomment-235963457
-    //     const didRenderSomething = () => elementToRender !== null;
+  //     //   // Use a closure to prevent the type from being incorrectly narrowed.
+  //     //   // https://github.com/microsoft/TypeScript/issues/9998#issuecomment-235963457
+  //     //   const didRenderSomething = () => elementToRender !== null;
     
-    //     for await (const frame of LLMx.partialRenderStream(current, shouldStop)) {
-    //       current = frame;
-    //       yield LLMx.debug(<DebugTree {...props}>{current}</DebugTree>);
-    //     }
+  //     //   for await (const frame of LLMx.partialRenderStream(current, shouldStop)) {
+  //     //     current = frame;
+  //     //     yield LLMx.debug(<DebugTree {...props}>{current}</DebugTree>);
+  //     //   }
     
-    //     if (!didRenderSomething()) {
-    //       break;
-    //     }
-    //   }
-    // }
-    // getAllFrames();
-  }, [componentToInspect]);
+  //     //   if (!didRenderSomething()) {
+  //     //     break;
+  //     //   }
+  //     // }
 
-  useInput((_input, key) => {
-    if (key.return) {
-      setFrameIndex(prevIndex => Math.min(steps.length - 1, prevIndex + 1));
-    }
-  });
+  //     push(LLMx.debug(<DebugTree>{componentToInspect}</DebugTree>))
+  //   }
+  //   getAllFrames();
+  // }, [componentToInspect]);
 
-  return <Text react color="green">{steps[frameIndex]} {frameIndex}</Text>;
+  // useInput((_input, key) => {
+  //   if (key.return) {
+  //     setFrameIndex(prevIndex => Math.min(steps.length - 1, prevIndex + 1));
+  //   }
+  // });
+
+  // return <Text react color="green">{steps[frameIndex]} {frameIndex}</Text>;
+  return <Text 
+    // @ts-expect-error
+    react 
+    color="green">{LLMx.debug(<DebugTree>{componentToInspect}</DebugTree>)}</Text>;
 };
 
 export function showInspector(componentToInspect: unknown) {
-  render(<Inspector react componentToInspect={componentToInspect} />);
+  render(<Inspector 
+    // @ts-expect-error
+    react 
+    componentToInspect={componentToInspect} />);
 }
