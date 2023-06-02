25f136319f84a100c85ba4802646fed44a54cd27
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-02 10:31:33 -0400

    Adwices

--- src/examples/bakeoff/zepp-health/ai-jsx.tsx
+++ src/examples/bakeoff/zepp-health/ai-jsx.tsx
@@ -8,6 +8,12 @@ import { loadJsonFile } from 'load-json-file';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+interface SleepQualityRatings {
+  SE: 'Low' | 'Moderate' | 'High';
+  SSO: 'Low' | 'Moderate' | 'High';
+  ISI: 'Low' | 'Moderate' | 'High';
+}
+
 function computeSleepQualityRatings(userData: typeof fixtureUserData) {
   let se = 0; // Initialize the number of nights with Sleep Efficiency < 80%
   let sso = 0; // Initialize the number of nights with Sleep Onset Duration > 30 minutes
@@ -60,17 +66,83 @@ function computeSleepQualityRatings(userData: typeof fixtureUserData) {
   };
 }
 
+function getAdvisorText(sleepQualityRatings: SleepQualityRatings): string {
+  const fallingAsleepAdvice = `
+    (1) limit the amount of time you spend trying to sleep by going to bed a
+    little later, closer to when you are falling asleep;
+    (2) if you cannot fall asleep in about 15-20 minutes, get out of bed and
+    engage in a quiet, screen-free activity in dim light until you start to feel
+    sleepy;
+  `;
+
+  const stayingAsleepAdvice = `
+    if you have trouble falling asleep at bedtime or wake up during the night
+    and cannot return to sleep after about 15-20 minutes, get out of bed and
+    engage in a quiet, screen-free activity in dim light until you start to feel
+    sleepy;
+  `;
+
+  const adjustingBedTimeAdvice = `
+    limit the amount of time you spend trying to sleep by going to bed a
+    little later, closer to when you are falling asleep or making sure that you
+    get out of bed shortly after waking in the morning;
+  `;
+
+  if (sleepQualityRatings.ISI === 'High') {
+    return 'Congratulations, you are sleeping well!';
+  }
+
+  let advice = 'You are at ';
+  advice += sleepQualityRatings.ISI === 'Moderate' ? 'moderate' : 'high';
+  advice += ' risk for insomnia. Consider trying the following to improve your sleep:\n';
+
+  if (sleepQualityRatings.ISI === 'Moderate') {
+    advice += fallingAsleepAdvice;
+  }
+
+  if (sleepQualityRatings.SSO === 'Moderate' || sleepQualityRatings.SSO === 'High') {
+    advice += adjustingBedTimeAdvice;
+  }
+
+  if (sleepQualityRatings.SE === 'Moderate' || sleepQualityRatings.SE === 'High') {
+    advice += stayingAsleepAdvice;
+  }
+
+  return advice;
+}
+
+async function getUserSleepAnalysis() {
+  const currentPath = path.dirname(fileURLToPath(import.meta.url));
+  const userData = (await loadJsonFile(path.join(currentPath, 'user-data.json'))) as typeof fixtureUserData;
+  return computeSleepQualityRatings(userData);
+}
+
 async function ShowStat({ query }: { query: string }) {
   // In the LangChain example, this was a Tool that the LLM decided to invoke. We don't need that here;
   // the decision has already been made by the router.
 
-  const currentPath = path.dirname(fileURLToPath(import.meta.url));
-  const userData = (await loadJsonFile(path.join(currentPath, 'user-data.json'))) as typeof fixtureUserData;
-  const analysis = computeSleepQualityRatings(userData);
+  const analysis = await getUserSleepAnalysis();
   return (
     <ChatCompletion>
       <SystemMessage>
         You are an expert sleep analyst. Here is some data about the user's sleep quality: {JSON.stringify(analysis)}
+      </SystemMessage>
+      <UserMessage>{query}</UserMessage>
+    </ChatCompletion>
+  );
+}
+
+async function ShowAdvice({ query }: { query: string }) {
+  const analysis = await getUserSleepAnalysis();
+  const adviceText = getAdvisorText(analysis);
+
+  return (
+    <ChatCompletion>
+      <SystemMessage>
+        You are an expert sleep analyst. Here is some data about the user's sleep quality: {JSON.stringify(analysis)}
+        Here is some info you know about how to improve this user's sleep quality: {adviceText}
+        Answer the user's question using the information above. If you can't answer it, apologize and say you're not
+        able to help.
       </SystemMessage>
       <UserMessage>{query}</UserMessage>
     </ChatCompletion>
@@ -89,7 +161,9 @@ function ZeppHealth({ query }: { query: string }) {
       <Route when="the user wants to know a specific stat about their sleep (ISI, SSO, or SE)">
         <ShowStat query={query} />
       </Route>
-      <Route when="the user wants advice about their sleep health">here is your advice</Route>
+      <Route when="the user wants advice about their sleep health">
+        <ShowAdvice query={query} />
+      </Route>
       <Route when="the user wants to see a summary of their sleep data">here is your summary</Route>
       <Route unmatched>I can't help with that.</Route>
     </NaturalLanguageRouter>
@@ -112,19 +186,19 @@ function AskAndAnswer({ query }: { query: string }) {
 
 showInspector(
   <>
-    <AskAndAnswer query="Please give me a recipe for cake" />
+    {/* <AskAndAnswer query="Please give me a recipe for cake" />
     <AskAndAnswer query="What does anthrax taste like?" />
     <AskAndAnswer query="What can you do?" />
-    <AskAndAnswer query="How can you help me?" />
+    <AskAndAnswer query="How can you help me?" /> */}
     <AskAndAnswer query="Can you provide me with information about my sleep quality ratings?" />
-    <AskAndAnswer query="What's my ISI rating?" />
+    {/* <AskAndAnswer query="What's my ISI rating?" />
     <AskAndAnswer query="What's my SSO rating?" />
-    <AskAndAnswer query="What's my SE rating?" />
-    <AskAndAnswer query="How can I get to sleep faster?" />
-    <AskAndAnswer query="Show me my sleep data as a markdown table" />
-    <AskAndAnswer query="Show me a chart about my sleep data" />
+    <AskAndAnswer query="What's my SE rating?" /> */}
+    {/* <AskAndAnswer query="How can I get to sleep faster?" /> */}
+    {/* <AskAndAnswer query="Show me my sleep data as a markdown table" />
+    <AskAndAnswer query="Show me a chart about my sleep data" /> */}
 
     {/* <AskAndAnswer query="What's my DOESNOTEXIST rating?" /> */}
   </>,
-  { showDebugTree: false }
+  { showDebugTree: true }
 );
