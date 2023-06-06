337ba8ca46cc298d8560009c55cd556987d178b2
juberti <justin@fixie.ai>
2023-06-06 11:57:15 -0700

    prettier

--- packages/ai-jsx/src/examples/summarizer.tsx
+++ packages/ai-jsx/src/examples/summarizer.tsx
@@ -1,11 +1,10 @@
 import { LLMx } from '../lib/index.ts';
 import { ChatCompletion, SystemMessage, UserMessage } from '../lib/completion-components.tsx';
 import { showInspector } from '../inspector/console.tsx';
-import fetch from "node-fetch"
+import fetch from 'node-fetch';
 
 //const BIG_TEXT = await fetch("https://norvig.com/big.txt").then((r) => r.text());
-const BIG_TEXT = 
-`Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty,
+const BIG_TEXT = `Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty,
 and dedicated to the proposition that all men are created equal.
 
 Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure.
@@ -18,28 +17,30 @@ we say here, but it can never forget what they did here. It is for us the living
 which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us,
 that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion, that we here
 highly resolve that these dead shall not have died in vain, that this nation, under God, shall have a new birth of freedom, and that
-government of the people, by the people, for the people, shall not perish from the earth.`
+government of the people, by the people, for the people, shall not perish from the earth.`;
 
 const tokenLen = (text: String) => text.length / 4;
 const MAX_TOKEN_LEN = 250;
 
-async function Summarizer({ children }: { children: LLMx.Node }, { render } : LLMx.RenderContext) {
+async function Summarizer({ children }: { children: LLMx.Node }, { render }: LLMx.RenderContext) {
   const text = await render(children);
   if (tokenLen(text) <= MAX_TOKEN_LEN) {
     return (
       <ChatCompletion>
         <SystemMessage>
-          Summarize the supplied text into a sentence. Only use the information
-          provided in the text; DO NOT use any information you know about the world.
+          Summarize the supplied text into a sentence. Only use the information provided in the text; DO NOT use any
+          information you know about the world.
         </SystemMessage>
         <UserMessage>{text}</UserMessage>
       </ChatCompletion>
     );
-  } 
+  }
 
   return (
-    <Summarizer> 
-      { text.split("\n\n").map(piece => <Summarizer>{piece}</Summarizer>) }
+    <Summarizer>
+      {text.split('\n\n').map((piece) => (
+        <Summarizer>{piece}</Summarizer>
+      ))}
     </Summarizer>
   );
 }
