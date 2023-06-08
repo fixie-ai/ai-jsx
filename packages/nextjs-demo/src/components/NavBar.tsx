feabb689b3a997f1e4dee59e9700191a6a9d1a24
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-08 11:09:45 -0400

    Fix lint

--- packages/nextjs-demo/src/components/NavBar.tsx
+++ packages/nextjs-demo/src/components/NavBar.tsx
@@ -4,7 +4,6 @@ import React, { Suspense } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import classNames from 'classnames';
-import Image from 'next/image';
 
 export default function NavBar() {
   const pathname = usePathname();
@@ -16,50 +15,51 @@ export default function NavBar() {
   };
 
   return (
-    <Suspense>
-      <nav className="bg-white shadow">
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <Image
-                    className="block h-6 w-auto lg:hidden"
-                    src="https://app.fixie.ai/static/logos/charcoal.png"
-                    alt="Your Company"
-                  />
-                  <Image
-                    className="hidden h-6 w-auto lg:block"
-                    src="https://app.fixie.ai/static/logos/charcoal.png"
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden sm:ml-6 sm:flex justify-center w-full sm:space-x-8">
-                  {Object.entries(pages).map(([name, href]) => {
-                    const active = pathname.startsWith(href);
-                    return (
-                      <Link
-                        key={href}
-                        href={href}
-                        className={classNames(
-                          'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                          {
-                            'border-indigo-500 text-gray-900': active,
-                            'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700': !active,
-                          }
-                        )}
-                      >
-                        {name}
-                      </Link>
-                    );
-                  })}
-                </div>
+    <nav className="bg-white shadow">
+      <>
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-shrink-0 items-center">
+                {/* We don't care about the perf for now. */}
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  className="block h-6 w-auto lg:hidden"
+                  src="https://app.fixie.ai/static/logos/charcoal.png"
+                  alt="Your Company"
+                />
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  className="hidden h-6 w-auto lg:block"
+                  src="https://app.fixie.ai/static/logos/charcoal.png"
+                  alt="Your Company"
+                />
+              </div>
+              <div className="hidden sm:ml-6 sm:flex justify-center w-full sm:space-x-8">
+                {Object.entries(pages).map(([name, href]) => {
+                  const active = pathname.startsWith(href);
+                  return (
+                    <Link
+                      key={href}
+                      href={href}
+                      className={classNames(
+                        'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                        {
+                          'border-indigo-500 text-gray-900': active,
+                          'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700': !active,
+                        }
+                      )}
+                    >
+                      {name}
+                    </Link>
+                  );
+                })}
               </div>
             </div>
           </div>
-        </>
-      </nav>
-    </Suspense>
+        </div>
+      </>
+    </nav>
   );
 }
