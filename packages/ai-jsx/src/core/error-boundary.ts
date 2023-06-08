1a4c8ffdd355f7d0ce7405411b818c27aeb0cb5b
Nick Heiner <NickHeiner@users.noreply.github.com>
2023-06-08 17:59:13 -0400

    Update packages/ai-jsx/src/core/error-boundary.ts
    Co-authored-by: Peter Salas <peter.salas@gmail.com>

--- packages/ai-jsx/src/core/error-boundary.ts
+++ packages/ai-jsx/src/core/error-boundary.ts
@@ -6,7 +6,7 @@ import { Node, RenderContext } from '../index.js';
  * If the fallback itself throws an error, that error will be propagated. (Just like if your `catch` block throws an error.)
  *
  * @example
- *  <ErrorBoundary fallback={User data could not be fetched.}>
+ *  <ErrorBoundary fallback="User data could not be fetched.">
  *    {fetchUserData()}
  *  </ErrorBoundary>
  *
