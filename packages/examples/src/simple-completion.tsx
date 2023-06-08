4a1f51d1cea380312fdad5eb5032e4e5b81bcf03
Peter Salas <peter@fixie.ai>
2023-06-08 12:32:53 -0700

    sample image renderer

--- packages/examples/src/simple-completion.tsx
+++ packages/examples/src/simple-completion.tsx
@@ -2,13 +2,25 @@ import * as LLMx from '@fixieai/ai-jsx';
 import { Completion } from '@fixieai/ai-jsx/core/completion';
 import { showInspector } from '@fixieai/ai-jsx/core/inspector';
 
-function App() {
-  return (
-    <Completion maxTokens={100}>
-      Q: Why is the sky blue?{'\n'}
-      A:
-    </Completion>
-  );
+const imageToString = LLMx.createContext<(src: string) => string>((x) => x);
+
+function Image({ src }: { src: string }, { getContext }: LLMx.RenderContext) {
+  const imageRenderer = getContext(imageToString);
+  return imageRenderer(src);
 }
 
-showInspector(<App />);
+function HTMLProvider({ children }: { children: LLMx.Node }) {
+  return <imageToString.Provider value={(src) => `<img src="${src}" />`}>{children}</imageToString.Provider>;
+}
+
+function App() {
+  return <Image src="http://foo/bar" />;
+}
+
+const ctx = LLMx.createRenderContext();
+const textAndImages = await ctx.render(
+  <HTMLProvider>
+    <App />
+  </HTMLProvider>
+);
+console.log(textAndImages);
