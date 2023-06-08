b5c1ef75f5fa17e736e12b3370fe96153d49b7fe
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-08 16:49:22 -0400

    Shorten the list

--- packages/nextjs-demo/src/app/react.ts
+++ packages/nextjs-demo/src/app/react.ts
@@ -5,17 +5,13 @@ import * as LLMx from '@fixieai/ai-jsx';
 
 // We would like to find a way to eliminate the need for this list.
 // This is only needed in the case where you are nesting React components inside your LLMx components.
-const knownLLMxTags = [
-  'ChatCompletion',
-  'UserMessage',
-  'SystemMessage',
-  'QueryGitHub',
-  'FetchGitHubGraphQL',
-  'NaturalLanguageGitHubSearch',
-  'DogNames',
-  'FormatAsHtml',
-  'FormatAsProse',
-  'ZeppHealth',
+const embeddableReactTags = [
+  'Recipe',
+  'RecipeTitle',
+  'RecipeInstructionList',
+  'RecipeInstructionListItem',
+  'RecipeIngredientList',
+  'RecipeIngredientListItem',
 ];
 
 function AIDehydrate({ reactElement }: { reactElement: any }) {
@@ -28,10 +24,10 @@ const monkeyPatchedReact = {
     const tag = args[0];
     const reactElement = React.createElement(...args);
     const llmxElement = LLMx.createElement(...args);
-    const isLLmxElement = typeof tag !== 'string' && knownLLMxTags.includes(tag.name);
-    const indirectNode = isLLmxElement
-      ? llmxElement
-      : LLMx.createElement(AIDehydrate, { reactElement, ...args[1] }, ...args.slice(2));
+    const isEmbeddableReactElement = typeof tag === 'string' || embeddableReactTags.includes(tag.name);
+    const indirectNode = isEmbeddableReactElement
+      ? LLMx.createElement(AIDehydrate, { reactElement, ...args[1] }, ...args.slice(2))
+      : llmxElement;
     LLMx.setIndirectNode(reactElement, indirectNode);
     return reactElement;
   },
