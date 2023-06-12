cda289e7c32ff141ac9e0ba8b00b1b7627fb818c
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-12 16:06:19 -0400

    This basically seems to work, although the model gets off track really easily

--- packages/create-react-app-demo/src/App.tsx
+++ packages/create-react-app-demo/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import * as LLMx from '@fixieai/ai-jsx';
 import React from 'react';
-import { AIRoot, conversationAtom } from './ai.tsx';
+import { AIRoot, ChatMessage, conversationAtom } from './ai.tsx';
 import { useState } from 'react';
 import { useAtom } from 'jotai';
 
@@ -24,24 +24,91 @@ import { useAtom } from 'jotai';
 // }
 
 function DebugConversation() {
-  const [userResponses] = useAtom(conversationAtom);
-  return <div>
-        <h1>Debug: Conversation as JSON</h1>
-        {userResponses.map((response, index) => {
-          return <div key={index}>{JSON.stringify(response)}</div>;
+  const [conversation] = useAtom(conversationAtom);
+  return (
+    <div>
+      <h1>Debug: Conversation as JSON</h1>
+      <ul>
+        {conversation.map((response, index) => {
+          return <li key={index}>{JSON.stringify(response)}</li>;
         })}
-      </div>
+      </ul>
+    </div>
+  );
+}
+
+function ConversationItem({ response, isLastResponse }: { response: ChatMessage; isLastResponse: boolean }) {
+  const [, setConversation] = useAtom(conversationAtom);
+
+  if (response.type === 'user') {
+    if (response.action === 'chat') {
+      return <div>User: {response.content}</div>;
+    }
+    return <div>User clicked: {response.id}</div>;
+  }
+  return (
+    <div>
+      AI:{' '}
+      {response.parts.flatMap((part, index) => {
+        if (part.type === 'text') {
+          return <span key={index}>{part.content}</span>;
+        }
+        if (part.type === 'ui') {
+          return part.content.map((row, index) => (
+            <li key={index}>
+              {row.map((button) => {
+                function handleClick() {
+                  setConversation((prev) => [
+                    ...prev,
+                    {
+                      type: 'user',
+                      action: 'click',
+                      id: button.id,
+                    },
+                  ]);
+                }
+                return (
+                  <button disabled={!isLastResponse} onClick={handleClick}>
+                    {button.text}
+                  </button>
+                );
+              })}
+            </li>
+          ));
+        }
+        return null;
+      })}
+    </div>
+  );
 }
 
 function ConversationHistory() {
-  const [userResponses] = useAtom(conversationAtom);
-
+  const [conversation] = useAtom(conversationAtom);
+  return (
+    <div>
+      <h1>Chat</h1>
+      {conversation.length ? (
+        <ul>
+          {conversation.map((response, index) => {
+            return (
+              <li key={index}>
+                <ConversationItem response={response} isLastResponse={index === conversation.length - 1} />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        'Loading...'
+      )}
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
       <AIRoot />
+      <ConversationHistory />
       <DebugConversation />
     </div>
   );
--- packages/create-react-app-demo/src/ai.tsx
+++ packages/create-react-app-demo/src/ai.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /** @jsx LLMx.createElement */
 /** @jsxFrag LLMx.Fragment */
 import * as LLMx from '@fixieai/ai-jsx';
@@ -13,25 +14,24 @@ interface BaseChatMessage {
   type: string;
 }
 interface UserResponse {
-  type: 'user',
+  type: 'user';
 }
 interface UserClick extends UserResponse {
-  action: 'click',
+  action: 'click';
   id: string;
 }
-interface UserChat extends BaseChatMessage {
-  action: 'chat',
+interface UserChat extends UserResponse {
+  action: 'chat';
   content: string;
 }
 interface AssistantChat extends BaseChatMessage {
-  type: 'assistant',
-  parts: Array<{ type: 'text', content: string } | {type: 'ui', content: z.infer<typeof Grid>}>;
+  type: 'assistant';
+  rawMessage: string;
+  parts: Array<{ type: 'text'; content: string } | { type: 'ui'; content: z.infer<typeof Grid> }>;
 }
-type AllChatMessages = AssistantChat | UserChat | UserClick;
+export type ChatMessage = AssistantChat | UserChat | UserClick;
 
-export const conversationAtom = atom<AllChatMessages>([] as AllChatMessages[]);
-
-// This needs better debouncing + only fire a new request when the user sends a message.
+export const conversationAtom = atom<ChatMessage[]>([]);
 
 const Button = z.object({
   id: z.string(),
@@ -57,14 +57,18 @@ function ButtonEnabledAgent({ conversation }: { conversation: any[] }) {
           click a button instead. For example, if you the user a question with a finite set of choices, give them
           buttons to make those choices.
         </SystemMessage>
-        <UserMessage>Let's play a game of chess</UserMessage>
-        {conversation.map(({ type, content, action }) => {
-          if (type === 'assissant') {
-            return <AssistantMessage>{content}</AssistantMessage>;
+        <UserMessage>Let's play tic tac toe</UserMessage>
+        {conversation.map((chatMessage) => {
+          if (chatMessage.type === 'assistant') {
+            return <AssistantMessage>{chatMessage.rawMessage}</AssistantMessage>;
           }
           return (
             <UserMessage>
-              {action === 'click' ? <>The user clicked button: {content}</> : <>The user said: {content}</>}
+              {chatMessage.action === 'click' ? (
+                <>The user clicked button: {chatMessage.id}</>
+              ) : (
+                <>The user said: {chatMessage.content}</>
+              )}
             </UserMessage>
           );
         })}
@@ -73,127 +77,69 @@ function ButtonEnabledAgent({ conversation }: { conversation: any[] }) {
   );
 }
 
-const sampleResponse = `TEXT: Sure! I'll set up the board. Please choose the color for your pieces.
-UI: [{"id":"white_pieces", "text":"White"},{"id":"black_pieces", "text":"Black"}]`;
-
-function useAI(children, dependencies, handleFinalResult, when: boolean) {
-  const isInProgressRef = useRef(false);
-  const [frame, setFrame] = useState('');
-
-  // useEffect(() => {
-  //   if (isInProgressRef.current || !when) {
-  //     return;
-  //   }
-  //   isInProgressRef.current = true;
-  //   LLMx.createRenderContext({
-  //     logger: console.log,
-  //   })
-  //     .render(children, {
-  //       map: (frame) => {
-  //         // debugger
-  //         setFrame(frame);
-  //       },
-  //     })
-  //     .then((finalFrame) => {
-  //       // debugger;
-  //       if (finalFrame) {
-  //         setFrame(finalFrame);
-  //         handleFinalResult({
-  //           type: 'assissant',
-  //           content: finalFrame,
-  //         });
-  //       }
-  //       isInProgressRef.current = false;
-  //     });
-  // }, [children, ...dependencies]);
-
-  return sampleResponse;
-}
-
 function AI() {
   const [conversation, setConversation] = useAtom(conversationAtom);
+  const isInProgressRef = useRef(false);
+
   const children = memo(<ButtonEnabledAgent conversation={conversation} />);
-  const frame = useAI(children, [_.last(conversation)?.type], finalResult => setConversation((prev) => [...prev, finalResult]), !conversation.length || _.last(conversation).type === 'user');
+  const when = !conversation.length || _.last(conversation).type === 'user';
 
-  // We need to show the entire history, not just the most recent AI response.
-  // Also, we need to show selected buttons.
-  return frame ? React.createElement(AIResponseToReact, { children: frame }, frame) : 'Loading...';
-}
+  function parseAIResponse(aiResponse: string) {
+    const regex = /(TEXT|UI):\s*([\s\S]*?)(?=(?:\sTEXT:|\sUI:|$))/g;
+    let match;
+    const result: AssistantChat['parts'] = [];
 
-function AIResponseToReact({ children: input }: { children: string }) {
-  const [conversation, setConversation] = useAtom(conversationAtom);
+    while ((match = regex.exec(aiResponse)) !== null) {
+      const type = match[1].toLowerCase() as 'text' | 'ui';
+      const content = match[2].trim();
+      if (type === 'text') {
+        result.push({ type, content });
+      } else {
+        let grid;
+        try {
+          grid = JSON.parse(content);
+        } catch {
+          // In this case, the UI part hasn't finished streaming yet, so we ignore it until it's done.
+          return null;
+        }
+        if (grid.type === 'Grid') {
+          grid = grid.value || grid.content;
+        }
+        if (Array.isArray(grid) && !Array.isArray(grid[0])) {
+          grid = [grid];
+        }
+        const validatedGrid = grid as z.infer<typeof Grid>;
+        console.log('got UI response', validatedGrid);
 
-  const regex = /(TEXT|UI):\s*([\s\S]*?)(?=(?:\sTEXT:|\sUI:|$))/g;
-  let match;
-  const result = [];
+        result.push({ type, content: grid, rawMessage: content });
+      }
+    }
 
-  while ((match = regex.exec(input)) !== null) {
-    result.push({ type: match[1], content: match[2].trim() });
+    setConversation((prev) => [
+      ...prev,
+      {
+        type: 'assistant',
+        parts: result,
+      },
+    ]);
   }
 
-  const children = result.map(({ type, content }, index) => {
-    if (type === 'TEXT') {
-      return React.createElement(
-        'p',
-        {
-          key: index,
-        },
-        content
-      );
+  useEffect(() => {
+    if (isInProgressRef.current || !when) {
+      return;
     }
-    if (type === 'UI') {
-      let grid;
-      try {
-        grid = JSON.parse(content);
-      } catch {
-        // In this case, the UI part hasn't finished streaming yet, so we ignore it until it's done.
-        return null;
-      }
-      if (grid.type === 'Grid') {
-        grid = grid.value || grid.content;
-      }
-      if (Array.isArray(grid) && !Array.isArray(grid[0])) {
-        grid = [grid];
-      }
-      const validatedGrid = grid as z.infer<typeof Grid>;
-      console.log('got UI response', validatedGrid);
-      return React.createElement(
-        'div',
-        {
-          key: index,
-        },
-        validatedGrid.map((row, index) => {
-          return React.createElement(
-            'div',
-            {
-              key: index,
-            },
-            row.map((button) => {
-              return React.createElement(
-                'button',
-                {
-                  onClick: () => {
-                    setConversation((prev) => [
-                      ...prev,
-                      {
-                        type: 'user',
-                        action: 'click',
-                        content: button.id,
-                      },
-                    ]);
-                  },
-                  key: button.id,
-                },
-                button.text
-              );
-            })
-          );
-        })
-      );
-    }
-  });
+    isInProgressRef.current = true;
+    LLMx.createRenderContext({
+      logger: console.log,
+    })
+      .render(children)
+      .then((finalFrame) => {
+        parseAIResponse(finalFrame);
+        isInProgressRef.current = false;
+      });
+  }, [children, _.last(conversation)?.type]);
 
-  return React.createElement(React.Fragment, { children });
+  return null;
 }
 
 export function AIRoot() {
