054a052304595a9bc5123ee0d5fff877770d915d
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-12 10:26:22 -0400

    Try a different way of specifying the output format

--- packages/examples/src/simple-chat.tsx
+++ packages/examples/src/simple-chat.tsx
@@ -3,7 +3,6 @@ import { ChatCompletion, SystemMessage, UserMessage } from '@fixieai/ai-jsx/core
 import { showInspector } from '@fixieai/ai-jsx/core/inspector';
 import { OpenAI } from '@fixieai/ai-jsx/lib/openai';
 import { z } from 'zod';
-import { zodToJsonSchema } from 'zod-to-json-schema';
 
 function App() {
   const Button = z.object({
@@ -23,7 +22,13 @@ function App() {
         follow this grammar: Split your response into logical chunks. Each chunk must start with either
         `TEXT:` or `UI:` If the line starts with `TEXT:`, the rest of the line is plain text that will be displayed to
         the user. If the line starts with `UI:`, the rest of the line is a JSON object representing UI that will be
-        shown to the user. This object should match this schema: {JSON.stringify(zodToJsonSchema(Grid))}. This gives you
+        shown to the user. This object should be of type `Grid`: 
+
+          type Button = {'{'} id: string; text: string {'}'};
+          type Row = Button[];
+          type Grid = Row[];
+        
+        . This gives you
         the ability to display a grid of buttons to the user. When the user clicks a button, you'll receive a message
         telling you which they clicked. Use your ability to show buttons to help the user accomplish their goal. Don't
         make the user type out a whole response if they can just click a button instead. For example, if you the user a question with a finite set of choices, give them buttons to make those choices.
@@ -34,5 +39,5 @@ function App() {
   );
 }
 
-console.log(await LLMx.createRenderContext().render(<App />));
-// showInspector(<App />);
+// console.log(await LLMx.createRenderContext().render(<App />));
+showInspector(<App />);
