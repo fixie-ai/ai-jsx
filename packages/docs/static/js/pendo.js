bbf534a41eb88113f4aeab86cde18a22d802795e
Matt Welsh <matt@fixie.ai>
2023-06-13 11:48:38 -0700

    Remove extra Pendo stuff.

--- packages/docs/static/js/pendo.js
+++ packages/docs/static/js/pendo.js
@@ -20,33 +20,5 @@
     z = e.getElementsByTagName(n)[0];
     z.parentNode.insertBefore(y, z);
   })(window, document, 'script', 'pendo');
-
-  // This function creates anonymous visitor IDs in Pendo unless you change the visitor id field to use your app's values
-  // This function uses the placeholder 'ACCOUNT-UNIQUE-ID' value for account ID unless you change the account id field to use your app's values
-  // Call this function after users are authenticated in your app and your visitor and account id values are available
-  // Please use Strings, Numbers, or Bools for value types.
-  pendo.initialize({
-    visitor: {
-      id: 'VISITOR-UNIQUE-ID', // Required if user is logged in, default creates anonymous ID
-      // email:        // Recommended if using Pendo Feedback, or NPS Email
-      // full_name:    // Recommended if using Pendo Feedback
-      // role:         // Optional
-
-      // You can add any additional visitor level key-values here,
-      // as long as it's not one of the above reserved names.
-    },
-
-    account: {
-      id: 'ACCOUNT-UNIQUE-ID', // Required if using Pendo Feedback, default uses the value 'ACCOUNT-UNIQUE-ID'
-      // name:         // Optional
-      // is_paying:    // Recommended if using Pendo Feedback
-      // monthly_value:// Recommended if using Pendo Feedback
-      // planLevel:    // Optional
-      // planPrice:    // Optional
-      // creationDate: // Optional
-
-      // You can add any additional account level key-values here,
-      // as long as it's not one of the above reserved names.
-    },
-  });
+  pendo.initialize({});
 })('94d07040-a56b-4f40-629a-eae64c0bb3ad');
