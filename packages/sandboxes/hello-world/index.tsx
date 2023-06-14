69836847423c377b6331f8e2ae78ac4f39c07ca5
Matt Welsh <matt@fixie.ai>
2023-06-14 14:28:49 -0700

    Formatting.

--- packages/sandboxes/hello-world/index.tsx
+++ packages/sandboxes/hello-world/index.tsx
@@ -1,27 +1,21 @@
-import * as LLMx from "ai-jsx";
-import {
-  ChatCompletion,
-  SystemMessage,
-  UserMessage,
-} from "ai-jsx/core/completion";
-import { pinoLogger } from "ai-jsx/core/log";
-import { pino } from "pino";
+import * as LLMx from 'ai-jsx';
+import { ChatCompletion, SystemMessage, UserMessage } from 'ai-jsx/core/completion';
+import { pinoLogger } from 'ai-jsx/core/log';
+import { pino } from 'pino';
 
 function App() {
   return (
     <ChatCompletion>
-      <SystemMessage>
-        You are an agent that only asks rhetorical questions.
-      </SystemMessage>
+      <SystemMessage>You are an agent that only asks rhetorical questions.</SystemMessage>
       <UserMessage>How can I learn about Ancient Egypt?</UserMessage>
     </ChatCompletion>
   );
 }
 const pinoStdoutLogger = pino({
-  name: "ai-jsx",
-  level: process.env.loglevel ?? "debug",
+  name: 'ai-jsx',
+  level: process.env.loglevel ?? 'debug',
   transport: {
-    target: "pino-pretty",
+    target: 'pino-pretty',
     options: {
       colorize: true,
     },
