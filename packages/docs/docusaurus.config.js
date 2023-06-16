23dde827d75e273c29470d5c63981d3048c8d81a
Matt Welsh <matt@fixie.ai>
2023-06-16 12:13:59 -0700

    Add Google Analytics to docs site.

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -47,6 +47,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-6EQLTL2L31',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
