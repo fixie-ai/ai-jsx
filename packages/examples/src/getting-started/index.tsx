e66fc02180a2405cd831808a815fa15018df4f43
Farzad Abdolhosseini <farzad.abdolhosseini@gmail.com>
2023-06-16 08:33:03 -0700

    Adding Dalle image generation (#43)
    * adding Dalle image generation
    
    * added image-gen example to getting-started

--- packages/examples/src/getting-started/index.tsx
+++ packages/examples/src/getting-started/index.tsx
@@ -3,7 +3,9 @@ import fs from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
 import * as LLMx from 'ai-jsx';
 import { ChatCompletion, SystemMessage, UserMessage } from 'ai-jsx/core/completion';
+import { ImageGen } from 'ai-jsx/core/image-gen';
 import { showInspector } from 'ai-jsx/core/inspector';
+import { memo } from 'ai-jsx/core/memoize';
 import { Node } from 'ai-jsx';
 
 function loadData() {
@@ -44,8 +46,6 @@ function Constitutional({ children }: { children: Node }) {
   );
 }
 
-// Disable the linter because this getting started file has two examples in one, and this one isn't used right now.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function WriteStory() {
   return (
     <ChatCompletion temperature={1}>
@@ -65,5 +65,18 @@ function WriteStory() {
   );
 }
 
-// showInspector(<WriteStory />)
-// console.log(await LLMx.createRenderContext().render(<WriteStory />));
+// Disable the linter because this getting started file has two examples in one, and this one isn't used right now.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function WriteStoryWithImage() {
+  const story = memo(<WriteStory />);
+  return (
+    <>
+      Banner URL: <ImageGen clipLongPrompt>Generate an image for this story: {story}</ImageGen>
+      {'\n\n'}
+      {story}
+    </>
+  );
+}
+
+// showInspector(<WriteStoryWithImage />)
+// console.log(await LLMx.createRenderContext().render(<WriteStoryWithImage />));
