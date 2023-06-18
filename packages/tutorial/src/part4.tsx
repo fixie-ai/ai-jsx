f1f3200e07090e6f0c835a071cb2d165491563a4
Matt Welsh <matt@fixie.ai>
2023-06-18 15:49:59 -0700

    Remove summarization example.

--- packages/tutorial/src/part4.tsx
+++ packages/tutorial/src/part4.tsx
@@ -4,7 +4,7 @@ import * as LLMx from 'ai-jsx';
 import { DocsQA, ScoredChunk, LocalCorpus, makeChunker, staticLoader } from 'ai-jsx/batteries/docs';
 import { showInspector } from 'ai-jsx/core/inspector';
 
-const URL = 'https://en.wikipedia.org/wiki/2005_Azores_subtropical_storm';
+const URL = 'https://en.wikipedia.org/wiki/Hurricane_Katrina';
 
 const html = await fetch(URL).then((response) => response.text());
 const turndownService = new TurndownService();
@@ -12,7 +12,7 @@ const markdown = turndownService.turndown(html);
 const docs = [
   {
     pageContent: [markdown],
-    name: 'Wikipedia Article about 2005 Azores subtropical storm',
+    name: 'Wikipedia Article about Hurricane Katrina',
   },
 ];
 const corpus = new LocalCorpus(staticLoader(docs), makeChunker(600, 100));
@@ -25,7 +25,7 @@ function ShowDoc({ doc }: { doc: ScoredChunk }) {
 function App() {
   return (
     <>
-      <DocsQA question="Summarize this article in a few sentences." corpus={corpus} limit={5} docComponent={ShowDoc} />
+      <DocsQA question="What was Hurricane Katrina?" corpus={corpus} limit={5} docComponent={ShowDoc} />
       {'\n\n'}
       <DocsQA question="Which dates did the storm occur?" corpus={corpus} limit={5} docComponent={ShowDoc} />
       {'\n\n'}
