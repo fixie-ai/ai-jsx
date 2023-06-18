f75d95192a39b0f73825465a1908d99a0ae3eaaf
Nick Heiner <NickHeiner@users.noreply.github.com>
2023-06-18 10:56:06 -0400

    Stream more in create-react-app (#70)
    This expands the UI bridge affordance, then updates the Docs Chat example to use it.
    
    As a result, we now have streaming results in Docs Chat. Docs Chat is also vastly simplified.
    
    This will cause some merge conflicts with #69 but I think we mostly changed different parts so it shouldn't be too bad to resolve.
    
    As a follow-up, it would be good to do this for the "Basic Chat" example as well.

--- packages/create-react-app-demo/src/docs-chat/ai.tsx
+++ packages/create-react-app-demo/src/docs-chat/ai.tsx
@@ -1,23 +1,9 @@
-/** @jsx LLMx.createElement */
-/** @jsxFrag LLMx.Fragment */
-/* eslint-disable react/jsx-key */
-import * as LLMx from 'ai-jsx';
-import React, { useEffect, useRef } from 'react';
+/** @jsx AI.createElement */
+/** @jsxFrag AI.Fragment */
+import * as AI from 'ai-jsx';
 import { DocsQA, ScoredChunk, LocalCorpus, defaultChunker, staticLoader } from 'ai-jsx/batteries/docs';
-import { memo } from 'ai-jsx/core/memoize';
 // @ts-expect-error
 import markdownPath from './brand-new.md';
-import { atom, useAtom } from 'jotai';
-
-import _ from 'lodash';
-
-export interface ChatMessage {
-  type?: string;
-  content?: string;
-}
-
-export const conversationAtom = atom<ChatMessage[]>([]);
-export const modelCallInProgress = atom<boolean>(false);
 
 // For now, we load the ai.jsx docs from a local markdown file. Once we have a HTML->Markdown converter,
 // we can load the docs from the site directly.
@@ -30,7 +16,8 @@ const docs = [
   },
 ];
 const corpus = new LocalCorpus(staticLoader(docs), defaultChunker);
-await corpus.startLoading();
+
+const corpusLoadedPromise = corpus.startLoading();
 
 const ShowDoc = ({ doc }: { doc: ScoredChunk }) => (
   <>
@@ -39,38 +26,7 @@ const ShowDoc = ({ doc }: { doc: ScoredChunk }) => (
   </>
 );
 
-function DocsAgent({ conversation }: { conversation: any[] }) {
-  const query = _.last(conversation)?.content;
-  return <DocsQA question={query} corpus={corpus} limit={5} docComponent={ShowDoc} />;
-}
-
-function AI() {
-  const [conversation, setConversation] = useAtom(conversationAtom);
-  const [, setCallInProgress] = useAtom(modelCallInProgress);
-  const isInProgressRef = useRef(false);
-  const children = memo(<DocsAgent conversation={conversation} />);
-  const when = conversation.length && _.last(conversation)?.type === 'user';
-
-  useEffect(() => {
-    if (isInProgressRef.current || !when) {
-      return;
-    }
-    setCallInProgress(true);
-    isInProgressRef.current = true;
-    // I couldn't get streaming to work here and I don't know why.
-    // Maybe because we're in the client and however Axios is doing it only works in Node?
-    LLMx.createRenderContext()
-      .render(children)
-      .then((finalFrame) => {
-        isInProgressRef.current = false;
-        setCallInProgress(false);
-        setConversation((prev) => [...prev, { type: 'assistant', content: finalFrame }]);
-      });
-  }, [children, setCallInProgress, when, setConversation]);
-
-  return null;
-}
-
-export function AIRoot() {
-  return React.createElement(AI, {});
+export async function DocsAgent({ question }: { question: string }) {
+  await corpusLoadedPromise;
+  return <DocsQA question={question} corpus={corpus} limit={5} docComponent={ShowDoc} />;
 }
