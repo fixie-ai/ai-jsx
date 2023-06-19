79e177a6e46171f68921fefbd6c71ee0e0538777
juberti <justin@fixie.ai>
2023-06-19 12:13:29 -0700

    v1

--- packages/create-react-app-demo/src/basic-chat/ai.tsx
+++ packages/create-react-app-demo/src/basic-chat/ai.tsx
@@ -1,22 +1,8 @@
 /** @jsxImportSource ai-jsx/react */
 /* eslint-disable react/jsx-key */
-import * as AI from 'ai-jsx';
-import React, { useEffect, useRef } from 'react';
 import { AssistantMessage, ChatCompletion, SystemMessage, UserMessage } from 'ai-jsx/core/completion';
-import { memo } from 'ai-jsx/core/memoize';
-import { atom, useAtom } from 'jotai';
 
-import _ from 'lodash';
-
-export interface ChatMessage {
-  type?: string;
-  content?: string;
-}
-
-export const conversationAtom = atom<ChatMessage[]>([]);
-export const modelCallInProgress = atom<boolean>(false);
-
-function ChatAgent({ conversation }: { conversation: any[] }) {
+export function ChatAgent({ conversation }: { conversation: any[] }) {
   return (
     <ChatCompletion>
       <SystemMessage>
@@ -24,8 +10,8 @@ function ChatAgent({ conversation }: { conversation: any[] }) {
         but Javascript is the true language of the people. When someone is talking to you, always try to steer the
         conversation back to these topics that you care so deeply about.
       </SystemMessage>
-      {conversation.map((chatMessage) =>
-        chatMessage.type === 'assistant' ? (
+      {conversation.map((chatMessage, index) =>
+        index % 2 != 0 ? (
           <AssistantMessage>{chatMessage.content}</AssistantMessage>
         ) : (
           <UserMessage>{chatMessage.content}</UserMessage>
@@ -33,31 +19,4 @@ function ChatAgent({ conversation }: { conversation: any[] }) {
       )}
     </ChatCompletion>
   );
-}
-
-export function AIRoot() {
-  const [conversation, setConversation] = useAtom(conversationAtom);
-  const [, setCallInProgress] = useAtom(modelCallInProgress);
-  const isInProgressRef = useRef(false);
-  const children = memo(<ChatAgent conversation={conversation} />);
-  const when = conversation.length && _.last(conversation)?.type === 'user';
-
-  useEffect(() => {
-    if (isInProgressRef.current || !when) {
-      return;
-    }
-    setCallInProgress(true);
-    isInProgressRef.current = true;
-    // I couldn't get streaming to work here and I don't know why.
-    // Maybe because we're in the client and however Axios is doing it only works in Node?
-    AI.createRenderContext()
-      .render(children)
-      .then((finalFrame) => {
-        isInProgressRef.current = false;
-        setCallInProgress(false);
-        setConversation((prev) => [...prev, { type: 'assistant', content: finalFrame }]);
-      });
-  }, [children, setCallInProgress, when, setConversation]);
-
-  return null;
 }
--- packages/create-react-app-demo/src/basic-chat/index.tsx
+++ packages/create-react-app-demo/src/basic-chat/index.tsx
@@ -1,32 +1,59 @@
-import React from 'react';
-import { AIRoot, ChatMessage, conversationAtom, modelCallInProgress } from './ai.tsx';
-import { useAtom } from 'jotai';
+/** @jsxImportSource ai-jsx/react */
+import * as AI from 'ai-jsx/react';
+import React, {useState} from 'react';
+import { ChatAgent } from './ai.tsx';
+import { useList } from 'react-use';
 import ResultContainer from '../ResultContainer.tsx';
 
-function ConversationItem({ response }: { response: ChatMessage }) {
-  const emoji = response.type === 'user' ? '👤' : '🤖';
+function ConversationItem({
+  responseType,
+  children: responseContent,
+}: {
+  responseType: 'user' | 'bot';
+  children: React.ReactNode;
+}) {
+  const emoji = responseType === 'user' ? '👤' : '🤖';
   return (
     <div>
-      {emoji}: {response.content}
+      {emoji}: {responseContent}
     </div>
   );
 }
 
-function ConversationHistory() {
-  const [conversation, setConversation] = useAtom(conversationAtom);
-  const [callInProgress] = useAtom(modelCallInProgress);
+/**
+ * We need to memoize this function. Otherwise, every time the parent component of AgentResponse re-renders,
+ * AgentReponse will re-render, which will trigger a new LLM call, which will produce a different result than the
+ * prior call.
+ */
+const AgentResponse = React.memo(function AgentResponse({
+  conversation,
+  setCallInProgress,
+}: {
+  conversation: any[];
+  setCallInProgress: (x: boolean) => void;
+}) {
+  return (
+    <ConversationItem responseType="bot">
+      <AI.jsx
+        onStreamStart={() => setCallInProgress(true)}
+        onStreamEnd={() => setCallInProgress(false)}
+        loading="Thinking..."
+      >
+        <ChatAgent conversation={conversation} />
+      </AI.jsx>
+    </ConversationItem>
+  );
+});
+
+export function BasicChat() {
+  const [userMessages, { push: pushUserMessage }] = useList<string>([]);
+  const [callInProgress, setCallInProgress] = useState(false);
 
   function handleInputSubmit(event: React.FormEvent<HTMLFormElement>) {
     // @ts-expect-error
     const element = event.target.elements.message;
     event.preventDefault();
-    setConversation((prev) => [
-      ...prev,
-      {
-        type: 'user',
-        content: element.value,
-      },
-    ]);
+    pushUserMessage(element.value);
 
     element.value = '';
   }
@@ -34,13 +61,15 @@ function ConversationHistory() {
   return (
     <ResultContainer title="Basic Chat" description="In this demo, you can chat with a quirky assistant.">
       <ul>
-        {conversation.map((response, index) => (
-          <li key={index} className="mt-4">
-            <ConversationItem response={response} />
-          </li>
-        ))}
-      </ul>
-      {callInProgress && <div>Waiting for AI response...</div>}
+        {userMessages.map((response, index) => [
+          <li key={`${index}-user`} className="mt-4">
+            <ConversationItem responseType="user">{response}</ConversationItem>
+          </li>,
+          <li key={`${index}-agent`} className="mt-4">
+            <AgentResponse conversation={userMessages} setCallInProgress={setCallInProgress} />
+          </li>,
+        ])}
+      </ul>      
       <form onSubmit={handleInputSubmit} className="mt-4 flex w-full">
         <input
           disabled={callInProgress}
@@ -57,14 +86,5 @@ function ConversationHistory() {
         </button>
       </form>
     </ResultContainer>
-  );
-}
-
-export function BasicChat() {
-  return (
-    <>
-      <AIRoot />
-      <ConversationHistory />
-    </>
   );
 }
