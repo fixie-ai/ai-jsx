32c5bafcee1641c1bb417146c5ebed08df6f601f
juberti <justin@fixie.ai>
2023-06-19 15:23:03 -0700

    cr

--- packages/create-react-app-demo/src/basic-chat/index.tsx
+++ packages/create-react-app-demo/src/basic-chat/index.tsx
@@ -1,8 +1,7 @@
 /** @jsxImportSource ai-jsx/react */
 import * as AI from 'ai-jsx/react';
-import { useAI } from 'ai-jsx/react';
 import React, { useState } from 'react';
-import { ChatAgent } from './ai.tsx';
+import { AssistantMessage, ChatCompletion, SystemMessage, UserMessage } from 'ai-jsx/core/completion';
 import { useList } from 'react-use';
 import ResultContainer from '../ResultContainer.tsx';
 
@@ -18,6 +17,21 @@ function ConversationItem({
     <div>
       {emoji}: {responseContent}
     </div>
+  );
+}
+
+function ChatAgent({ conversation }: { conversation: string[] }) {
+  return (
+    <ChatCompletion>
+      <SystemMessage>
+        You are a web developer who is passionate about HTML, CSS, and JS/JSX. You think that other languages are fine,
+        but Javascript is the true language of the people. When someone is talking to you, always try to steer the
+        conversation back to these topics that you care so deeply about.
+      </SystemMessage>
+      {conversation.map((message, index) =>
+        index % 2 ? <AssistantMessage>{message}</AssistantMessage> : <UserMessage>{message}</UserMessage>
+      )}
+    </ChatCompletion>
   );
 }
 
