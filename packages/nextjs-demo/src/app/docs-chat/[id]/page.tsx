f465f97b600e02be2853049370d373ea93e9e8e9
Peter Salas <peter@fixie.ai>
2023-06-19 15:50:13 -0700

    stash work to share

--- packages/nextjs-demo/src/app/docs-chat/[id]/page.tsx
+++ packages/nextjs-demo/src/app/docs-chat/[id]/page.tsx
@@ -80,13 +80,7 @@ export default async function DocsChat({
   params: { id: string };
   searchParams: { message?: string };
 }) {
-  let conversationHistory = await getConversation(params.id);
-
-  if (conversationHistory.length === 0) {
-    await appendConversation(params.id, { author: 'user', message: 'What is AI.JSX?' });
-    conversationHistory = await getConversation(params.id);
-  }
-
+  const conversationHistory = await getConversation(params.id);
   return (
     <ResultContainer
       title="Docs Chat"
@@ -104,7 +98,7 @@ export default async function DocsChat({
         {conversationHistory.map((response, index) => (
           <ConversationItem responseType={response.author}>{response.message}</ConversationItem>
         ))}
-        {conversationHistory[conversationHistory.length - 1].author == 'user' && (
+        {conversationHistory.length > 0 && conversationHistory[conversationHistory.length - 1].author == 'user' && (
           <>
             <ConversationItem responseType="bot">
               <Suspense fallback="⎕">
@@ -127,6 +121,7 @@ export default async function DocsChat({
           name="message"
           placeholder="Ask a question..."
           className="w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          defaultValue={conversationHistory.length === 0 ? 'What is AI.JSX?' : ''}
         />
         <button
           type="submit"
