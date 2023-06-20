a9984b8502687157250c1dd759642245fbb0eb8f
Matt Welsh <matt@fixie.ai>
2023-06-20 09:18:43 -0700

    Adding openaiproxy edge function.

--- packages/tutorial-nextjs/src/app/api/hello/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET(Request) {
-  return new Response('Hello from your API handler');
-}
---
+++ packages/tutorial-nextjs/src/app/api/openaiproxy/route.ts
@@ -0,0 +1,39 @@
+/**
+ * This code is a Vercel Edge Function that proxies requests to the OpenAI API.
+ * This is used to protect your OpenAI API key from being exposed to the client.
+ * Code is adapted from: https://sdk.vercel.ai/
+ */
+
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+export const runtime = 'edge';
+
+const apiConfig = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY!,
+});
+
+const openai = new OpenAIApi(apiConfig);
+
+export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
+  const { messages } = await req.json();
+
+  // Request the OpenAI API for the response based on the prompt
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: messages,
+    max_tokens: 500,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
