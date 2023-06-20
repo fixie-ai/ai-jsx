2632256dfb28c706ac7e3f78f7995ae07e42cfdc
Mike Depinet <mike@fixie.ai>
2023-06-20 16:06:07 -0700

    Adapt to jest being run by turbo

--- packages/ai-jsx/jest.config.ts
+++ packages/ai-jsx/jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
 
 export default config;
