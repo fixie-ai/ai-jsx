f31668d6be6cae0e205e5b46d246a5fbf4a39b28
Matt Welsh <matt@fixie.ai>
2023-06-20 12:33:23 -0700

    Fix formatting of CRA demo.

--- packages/create-react-app-demo/src/About.tsx
+++ packages/create-react-app-demo/src/About.tsx
@@ -2,29 +2,33 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className="flex flex-col">
-      <div className="h-6 flex-row w-full"></div>
-      <div className="flex flex-row w-full">
-        <div className="w-1/5"></div>
-        <div className="w-3/5">
-          <div className="p-6 bg-white shadow font-medium text-gray-500 text-lg">
-            This is a demo of{' '}
-            <a className="underline" href="https://ai-jsx.com/">
-              AI.JSX
-            </a>{' '}
-            using React. AI.JSX is a JavaScript framework for building powerful applications with Large Language Models.
-            For more details, checkout the{' '}
-            <a className="underline" href="https://docs.ai-jsx.com/">
-              AI.JSX documentation
-            </a>
-            , or the{' '}
-            <a className="underline" href="https://github.com/fixie-ai/ai-jsx/tree/main/packages/create-react-app-demo">
-              source code for this app
-            </a>
-            .
+    <div className="flex flex-col items-start px-24 py-6">
+      <div className="min-w-full max-w-7xl">
+        <div className="min-w-full max-w-none">
+          <div className="overflow-hidden bg-white sm:rounded-lg sm:shadow">
+            <div className="whitespace-pre-wrap bg-white px-4 py-5 sm:px-6">
+              <p>
+                This is a demo of{' '}
+                <a className="underline" href="https://ai-jsx.com/">
+                  AI.JSX
+                </a>{' '}
+                using React. AI.JSX is a JavaScript framework for building powerful applications with Large Language
+                Models. For more details, checkout the{' '}
+                <a className="underline" href="https://docs.ai-jsx.com/">
+                  AI.JSX documentation
+                </a>
+                , or the{' '}
+                <a
+                  className="underline"
+                  href="https://github.com/fixie-ai/ai-jsx/tree/main/packages/create-react-app-demo"
+                >
+                  source code for this app
+                </a>
+                .
+              </p>
+            </div>
           </div>
         </div>
-        <div className="w-1/5"></div>
       </div>
     </div>
   );
