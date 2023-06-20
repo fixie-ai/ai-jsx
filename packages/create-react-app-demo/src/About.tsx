7e71d457fa3d9ccc69feea8cf3a221da2ff22ccc
Matt Welsh <matt@fixie.ai>
2023-06-20 11:12:12 -0700

    Add AI.JSX branding and an 'About' box to the CRA demo.

---
+++ packages/create-react-app-demo/src/About.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classNames from 'classnames';
+
+export default function About() {
+  return (
+    <div className="flex flex-col">
+      <div className="h-6 flex-row w-full"></div>
+      <div className="flex flex-row w-full">
+        <div className="w-1/5"></div>
+        <div className="w-3/5">
+          <div className="p-6 bg-white shadow font-medium text-gray-500 text-lg">
+            This is a demo of{' '}
+            <a className="underline" href="https://ai-jsx.com/">
+              AI.JSX
+            </a>{' '}
+            using React. AI.JSX is a JavaScript framework for building powerful applications with Large Language Models.
+            For more details, checkout the{' '}
+            <a className="underline" href="https://docs.ai-jsx.com/">
+              AI.JSX documentation
+            </a>
+            , or the{' '}
+            <a className="underline" href="https://github.com/fixie-ai/ai-jsx/tree/main/packages/create-react-app-demo">
+              source code for this app
+            </a>
+            .
+          </div>
+        </div>
+        <div className="w-1/5"></div>
+      </div>
+    </div>
+  );
+}
--- packages/create-react-app-demo/src/layout.tsx
+++ packages/create-react-app-demo/src/layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import NavBar from './NavBar.tsx';
+import About from './About.tsx';
 import { Outlet } from 'react-router-dom';
 
 export const metadata = {
-  title: 'AI.JSX + NextJS SSR Demo',
+  title: 'AI.JSX React App Demo',
   description: 'A framework for AI-native UIs',
 };
 
@@ -11,6 +12,7 @@ export default function RootLayout({ children = <Outlet /> }) {
   return (
     <>
       <NavBar />
+      <About />
       <main className="flex min-h-screen flex-col items-start p-24">{children}</main>
     </>
   );
