b4df0564d29e36045a1b5354b12d57d45a82ea6e
Mike Depinet <mike@fixie.ai>
2023-06-21 09:26:34 -0700

    Working, but VS code isn't happy

--- packages/ai-jsx/jest.config.ts
+++ packages/ai-jsx/jest.config.ts
@@ -4,6 +4,9 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleDirectories: ['node_modules', 'dist', 'src'],
+  modulePaths: ['<rootDir>/node_modules', '<rootDir>/dist', '<rootDir>/src'],
+  roots: ['<rootDir>', '<rootDir>/src', '<rootDir>/dist'],
 };
 
 export default config;
--- packages/ai-jsx/src/batteries/docs.test.ts
+++ packages/ai-jsx/src/batteries/docs.test.ts
@@ -1,3 +1,44 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+import { RawLoader, staticLoader, toLoader } from '../../dist/cjs/batteries/docs.cjs';
+
+test('defaultParser handles simple text', async () => {
+  const rawDoc = {
+    pageContent: new TextEncoder().encode('Hello world!'),
+    name: 'greeting.txt',
+    encoding: 'utf-8',
+    mimeType: 'text/plain',
+  };
+  const rawLoader: RawLoader = (_) => Promise.resolve({ page: { documents: [rawDoc] } });
+  const loader = toLoader(rawLoader);
+  const parsedDoc = (await loader({})).page!.documents[0];
+  expect(parsedDoc).toEqual({
+    pageContent: ['Hello world!'],
+    name: 'greeting.txt',
+  });
 });
+
+test('staticLoader supports pagination', async () => {
+  const docs = [
+    { name: 'doc1.txt', pageContent: ['Hello world1!'] },
+    { name: 'doc2.txt', pageContent: ['Hello world2!'] },
+    { name: 'doc3.txt', pageContent: ['Hello world3!'] },
+    { name: 'doc4.txt', pageContent: ['Hello world4!'] },
+    { name: 'doc5.txt', pageContent: ['Hello world5!'] },
+  ];
+  const loader = staticLoader(docs, 2);
+  const response1 = await loader({});
+  expect(response1.page!.documents).toEqual([docs[0], docs[1]]);
+  expect(response1.partitions).toBeUndefined();
+  const response2 = await loader({ pageToken: response1.page!.nextPageToken });
+  expect(response2.page!.documents).toEqual([docs[2], docs[3]]);
+  expect(response2.partitions).toBeUndefined();
+  const response3 = await loader({ pageToken: response2.page!.nextPageToken });
+  expect(response3.page!.documents).toEqual([docs[4]]);
+  expect(response3.page!.nextPageToken).toBeUndefined();
+  expect(response3.partitions).toBeUndefined();
+});
+
+// Corpus loading (custom chunker and embedding), local and LC
+// Query for not read local corpus
+// Query for local corpus
+// Query for LC corpus
+// Ideally DocsQA component if we can mock out ChatCompletion
