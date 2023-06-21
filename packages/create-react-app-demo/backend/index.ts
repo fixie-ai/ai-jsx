d43893032d736fa6fc4039e38b6ba991e9b89099
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-21 14:37:51 -0700

    Comment

--- packages/create-react-app-demo/backend/index.ts
+++ packages/create-react-app-demo/backend/index.ts
@@ -1,6 +1,12 @@
 import httpProxy from 'http-proxy';
 import http from 'node:http';
 
+/**
+ * This is a demo proxy server that allows you to use the OpenAI API without exposing your key to the client.
+ *
+ * If you deploy this, you may wish to expand on the proxy to deny-list endpoints your app doesn't use, to prevent abuse by third parties.
+ */
+
 if (!process.env.OPENAI_API_KEY) {
   throw new Error('OPENAI_API_KEY environment variable must be set');
 }
