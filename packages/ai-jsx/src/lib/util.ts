08bddd8640a2983ae7c7884255a63f4e96d7e0f1
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-21 16:50:44 -0700

    Fix lint

--- packages/ai-jsx/src/lib/util.ts
+++ packages/ai-jsx/src/lib/util.ts
@@ -1,5 +1,8 @@
 /** @hidden */
 export function getEnvVar(name: string, shouldThrow: boolean = true) {
+  // We actually want the nullish coalescing behavior in this case,
+  // because we want to treat '' as undefined.
+  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
   return getEnvVarByName(name, shouldThrow) || getEnvVarByName(`REACT_APP_${name}`, shouldThrow);
 }
 
