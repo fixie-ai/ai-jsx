06258089864b306c4569676e45842060cfe442b6
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-21 14:01:45 -0700

    Restore file to main

--- packages/create-react-app-demo/scripts/start.cjs
+++ packages/create-react-app-demo/scripts/start.cjs
@@ -102,7 +102,7 @@ checkBrowsers(paths.appPath, isInteractive)
       }
 
       console.log(chalk.cyan('Starting the development server...\n'));
-      // openBrowser(urls.localUrlForBrowser);
+      openBrowser(urls.localUrlForBrowser);
     });
 
     ['SIGINT', 'SIGTERM'].forEach(function (sig) {
