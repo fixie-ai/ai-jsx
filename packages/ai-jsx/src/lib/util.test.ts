ac64fc751532373a56851c9c49c638025ab47c51
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-22 12:47:22 -0700

    Add tests and fix logic

---
+++ packages/ai-jsx/src/lib/util.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@jest/globals';
+import { getEnvVar } from "./util";
+
+process.env.EXISTS = 'exists'
+process.env.REACT_APP_ONLY = 'react-value'
+
+test('env var exists under original name', () => {
+  expect(getEnvVar('EXISTS')).toEqual('exists')
+})
+
+test('env var exists under react app name', () => {
+  expect(getEnvVar('ONLY')).toEqual('react-value')
+});
+
+test('env var does not exist', () => {
+  expect(() => getEnvVar('DOES_NOT_EXIST')).toThrowError(/Please specify env var 'DOES_NOT_EXIST' or 'REACT_APP_DOES_NOT_EXIST'./)
+});
+
+test('env var does not exist and shouldThrow=false', () => {
+  expect(getEnvVar('DOES_NOT_EXIST', false)).toBeUndefined();
+});
