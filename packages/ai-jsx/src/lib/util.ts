a1a293c4df92d3ab03fe110bc7b0318c30c1362f
Nick Heiner <NickHeiner@users.noreply.github.com>
2023-06-22 13:27:45 -0700

    Fix env var logic (#139)
    Previously, the `shouldThrow` logic was too aggressive, and the env var lookup would throw an error if `FOO` didn't exist, even if `REACT_APP_FOO` did.

--- packages/ai-jsx/src/lib/util.ts
+++ packages/ai-jsx/src/lib/util.ts
@@ -1,15 +1,12 @@
 /** @hidden */
 export function getEnvVar(name: string, shouldThrow: boolean = true) {
+  const reactAppName = `REACT_APP_${name}`;
   // We actually want the nullish coalescing behavior in this case,
   // because we want to treat '' as undefined.
   // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-  return getEnvVarByName(name, shouldThrow) || getEnvVarByName(`REACT_APP_${name}`, shouldThrow);
-}
-
-function getEnvVarByName(name: string, shouldThrow: boolean) {
-  const value = process.env[name];
-  if (!value && shouldThrow) {
-    throw new Error(`Please specify env var "${name}".`);
+  const result = process.env[name] || process.env[reactAppName];
+  if (result === undefined && shouldThrow) {
+    throw new Error(`Please specify env var '${name}' or '${reactAppName}'.`);
   }
-  return value;
+  return result;
 }
