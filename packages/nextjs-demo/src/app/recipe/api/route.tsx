c1495b03390e2578eaaf96f05e5415c5d59a415c
Nick Heiner <nick.heiner+github@gmail.com>
2023-06-29 23:44:57 -0400

    Hack to be able to iterate on the UI faster

--- packages/nextjs-demo/src/app/recipe/api/route.tsx
+++ packages/nextjs-demo/src/app/recipe/api/route.tsx
@@ -6,6 +6,7 @@ import { UICompletion } from 'ai-jsx/react/completion';
 import RecipeMap from '@/components/Recipe.map';
 import { memo } from 'ai-jsx/core/memoize';
 import { ImageGen } from 'ai-jsx/core/image-gen';
+import { fileURLToPath } from 'node:url';
 const {
   Recipe,
   RecipeIngredientList,
@@ -14,6 +15,8 @@ const {
   RecipeInstructionListItem,
   RecipeTitle,
 } = RecipeMap;
+import fs from 'fs';
+import path from 'path';
 
 export async function POST(request: NextRequest) {
   const { topic } = await request.json();
@@ -24,42 +27,52 @@ export async function POST(request: NextRequest) {
       <UserMessage>Give me a recipe for {topic}.</UserMessage>
     </ChatCompletion>
   );
-  return AI.toReactStream(
-    RecipeMap,
-    <>
-      <ChatCompletion>
-        <SystemMessage>
-          The user will ask for you a recipe. Tell them you'd be happy to do that. Pretend to be an expert in whatever
-          culture the recipe is from. Respond in 1-3 sentences. Do not ask a question.
-        </SystemMessage>
-        <UserMessage>I'd like a recipe about {topic}</UserMessage>
-      </ChatCompletion>
-      <ImageGen size="256x256">
-        <ChatCompletion>
-          <UserMessage>Summarize the following recipe into a two sentence description: {recipe}</UserMessage>
-        </ChatCompletion>
-      </ImageGen>
-      <UICompletion
-        example={
-          <Recipe>
-            <RecipeTitle>Crème Chantilly</RecipeTitle>
-            <RecipeIngredientList>
-              <RecipeIngredientListItem>2 cups heavy cream</RecipeIngredientListItem>
-              <RecipeIngredientListItem>2 tablespoons granulated sugar</RecipeIngredientListItem>
-              <RecipeIngredientListItem>1 teaspoon vanilla extract</RecipeIngredientListItem>
-            </RecipeIngredientList>
-            <RecipeInstructionList>
-              <RecipeInstructionListItem>Combine the ingredients in a large mixing bowl.</RecipeInstructionListItem>
-              <RecipeInstructionListItem>
-                Beat the contents on high speed until soft peaks form.
-              </RecipeInstructionListItem>
-              <RecipeIngredientListItem>Keep chilled until serving.</RecipeIngredientListItem>
-            </RecipeInstructionList>
-          </Recipe>
-        }
-      >
-        {recipe}
-      </UICompletion>
-    </>
-  );
+
+  const textEncoder = new TextEncoder();
+  const fakeStream = fs.readFileSync(path.join(process.cwd(), 'src', 'app', 'recipe', 'api', 'fixture.txt'), 'utf-8');
+  return new Response(new ReadableStream({
+    start(controller) {
+      controller.enqueue(textEncoder.encode(fakeStream));
+      controller.close();
+    }
+  }))
+
+  // return AI.toReactStream(
+  //   RecipeMap,
+  //   <>
+  //     <ChatCompletion>
+  //       <SystemMessage>
+  //         The user will ask for you a recipe. Tell them you'd be happy to do that. Pretend to be an expert in whatever
+  //         culture the recipe is from. Respond in 1-3 sentences. Do not ask a question.
+  //       </SystemMessage>
+  //       <UserMessage>I'd like a recipe about {topic}</UserMessage>
+  //     </ChatCompletion>
+  //     <ImageGen size="256x256">
+  //       <ChatCompletion>
+  //         <UserMessage>Summarize the following recipe into a two sentence description: {recipe}</UserMessage>
+  //       </ChatCompletion>
+  //     </ImageGen>
+  //     <UICompletion
+  //       example={
+  //         <Recipe>
+  //           <RecipeTitle>Crème Chantilly</RecipeTitle>
+  //           <RecipeIngredientList>
+  //             <RecipeIngredientListItem>2 cups heavy cream</RecipeIngredientListItem>
+  //             <RecipeIngredientListItem>2 tablespoons granulated sugar</RecipeIngredientListItem>
+  //             <RecipeIngredientListItem>1 teaspoon vanilla extract</RecipeIngredientListItem>
+  //           </RecipeIngredientList>
+  //           <RecipeInstructionList>
+  //             <RecipeInstructionListItem>Combine the ingredients in a large mixing bowl.</RecipeInstructionListItem>
+  //             <RecipeInstructionListItem>
+  //               Beat the contents on high speed until soft peaks form.
+  //             </RecipeInstructionListItem>
+  //             <RecipeIngredientListItem>Keep chilled until serving.</RecipeIngredientListItem>
+  //           </RecipeInstructionList>
+  //         </Recipe>
+  //       }
+  //     >
+  //       {recipe}
+  //     </UICompletion>
+  //   </>
+  // );
 }
