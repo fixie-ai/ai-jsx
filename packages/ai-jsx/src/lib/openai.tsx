59a1fdeb43f79a7b55f0f344acca586ad60f8921
Farzad Abdolhosseini <farzad@fixie.ai>
2023-07-05 16:10:35 -0700

    revert unnecessary change

--- packages/ai-jsx/src/lib/openai.tsx
+++ packages/ai-jsx/src/lib/openai.tsx
@@ -414,7 +414,7 @@ export async function* OpenAIChatModel(
     }
   }
 
-  logger.info({ message: currentMessage }, 'Finished createChatCompletion');
+  logger.debug({ message: currentMessage }, 'Finished createChatCompletion');
 
   if (currentMessage.function_call) {
     yield (
