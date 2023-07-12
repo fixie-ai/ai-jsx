415e760ec63a312e84403546902b533500427c48
Farzad Abdolhosseini <farzad@fixie.ai>
2023-07-12 16:41:59 -0700

    UICompletion interface: 1-shot + docs vs examples

--- packages/ai-jsx/src/react/completion.tsx
+++ packages/ai-jsx/src/react/completion.tsx
@@ -14,9 +14,174 @@ function reactComponentName(component: AI.Component<any> | React.JSXElementConst
 }
 
 interface SerializedComponent {
-  name: string;
-  children: (string | SerializedComponent)[];
+  tag: string;
+  children: string | (string | SerializedComponent)[];
   possiblyIncomplete?: boolean;
+}
+
+type ComponentType = AI.Component<any> | React.JSXElementConstructor<any> | string;
+
+export async function* UICompletion(
+  {
+    reactComponentsDoc,
+    aiComponentsDoc,
+    children,
+  }: {
+    reactComponentsDoc?: React.ReactNode;
+    aiComponentsDoc?: React.ReactNode;
+    children: AI.Node;
+  },
+  { render, logger }: AI.ComponentContext
+) {
+  yield '';
+
+  // TODO: what does the inReact flag do?
+  const aiComponents = collectComponents(aiComponentsDoc, true);
+  const validAIComponentsMap = Object.fromEntries(Array.from(aiComponents).map((c) => [reactComponentName(c), c]));
+
+  const reactComponents = collectComponents(reactComponentsDoc, true);
+
+  const allComponents = [...Array.from(reactComponents), ...Array.from(aiComponents)];
+  const validComponentsMap = Object.fromEntries(allComponents.map((c) => [reactComponentName(c), c]));
+
+  const memoizedComponents = new Map<string, AI.Node>();
+
+  function getOrCreateMemo(serializedComponent: SerializedComponent, nonMemoized: AI.Node): AI.Node {
+    const hash = CryptoJS.SHA256(JSON.stringify(serializedComponent)).toString();
+    if (!memoizedComponents.has(hash)) {
+      memoizedComponents.set(hash, memo(nonMemoized));
+    }
+    return memoizedComponents.get(hash);
+  }
+
+  // TODO: refactor this out of the component.
+  function toComponent(serializedComponent: SerializedComponent[] | SerializedComponent | string) {
+    if (!serializedComponent || typeof serializedComponent === 'string') {
+      return serializedComponent;
+    }
+    if (Array.isArray(serializedComponent)) {
+      return <AI.Fragment>{serializedComponent.map(toComponent)}</AI.Fragment>;
+    }
+
+    const Component = validComponentsMap[serializedComponent.tag];
+    if (!Component) {
+      logger.warn(
+        { serializedComponent },
+        `Ignoring component "${serializedComponent.tag}" that wasn't present in the example. ` +
+          'You may need to adjust the prompt or include an example of this component.'
+      );
+      return null;
+    }
+
+    if (!('children' in serializedComponent)) {
+      throw new AIJSXError(
+        `JSON produced by the model did not fit the required schema: ${JSON.stringify(serializedComponent)}`,
+        ErrorCode.ModelOutputDidNotMatchUIShape,
+        'runtime'
+      );
+    }
+
+    // Sometimes the model returns a singleton string instead of an array.
+    const children =
+      typeof serializedComponent.children === 'string' ? [serializedComponent.children] : serializedComponent.children;
+
+    if (serializedComponent.tag in validAIComponentsMap) {
+      if (serializedComponent.possiblyIncomplete) {
+        return `Loading ${serializedComponent.tag} ...`;
+      }
+      return (
+        <AI.jsx>{getOrCreateMemo(serializedComponent, <Component>{children.map(toComponent)}</Component>)}</AI.jsx>
+      );
+    }
+    return (
+      <AI.React>
+        <Component>{children.map(toComponent)}</Component>
+      </AI.React>
+    );
+  }
+
+  const Element: z.Schema = z.object({
+    tag: z.string().refine((c) => Boolean(validComponentsMap[c]), {
+      message: `Unknown component "tag". Supported components: ${Object.keys(validComponentsMap)}`,
+    }),
+    children: z.union([z.string(), z.array(z.union([z.string(), z.lazy(() => Element)]))]),
+  });
+  const Elements = z.union([Element, z.array(Element)]);
+
+  const modelRenderGenerator = render(
+    <JsonChatCompletion schema={Elements} retries={1}>
+      <SystemMessage>
+        You are an AI who is an expert UI designer. You can describe the UI as a nested JSON object. The JSON will be
+        used to create a React/HTML UI tree. Each component is described using a "tag" name and a list of "children".
+        Each child can be either a string or another component.
+        {'For example <SomeComponent /> becomes { "tag": "SomeComponent", "children": [] }'}
+        {'\n'}
+        The user will ask you a question and your job is to use the set of React components below to create a UI that
+        the query asks for.
+        {'\n'}
+        Here's a list of the components that are available to you and their documentation:
+        {'\n```txt filename="component_docs.txt"\n'}
+        <AI.React>{reactComponentsDoc}</AI.React>
+        {'\n'}
+        <AI.React>{aiComponentsDoc}</AI.React>
+        {'\n```\n'}
+        Do not use any elements (including HTML elements) other than the ones above. As such, the "tag" name of the
+        component can only be one of the following: {Object.keys(validComponentsMap).join(', ')}. Nothing else is
+        permitted.
+      </SystemMessage>
+      <UserMessage>{children}</UserMessage>
+    </JsonChatCompletion>
+  )[Symbol.asyncIterator]();
+
+  while (true) {
+    const modelResult = await modelRenderGenerator.next();
+    const object = JSON.parse(modelResult.value);
+    if (!modelResult.done) {
+      markLastElementIncomplete(object);
+    }
+    const component = toComponent(object);
+    if (modelResult.done) {
+      return component;
+    }
+    yield component;
+  }
+}
+
+/**
+ * Given a node, it will recursively traverse the tree and collect all the components that are used.
+ * This is used to create a list of available components for the LLM to use.
+ */
+function collectComponents(
+  node: React.ReactNode | AI.Node | undefined,
+  inReact: boolean,
+  componentsList: Set<ComponentType> = new Set<ComponentType>()
+): Set<ComponentType> {
+  if (!node) {
+    return componentsList;
+  }
+  if (Array.isArray(node)) {
+    node.forEach((node) => collectComponents(node, inReact, componentsList));
+  }
+
+  if (React.isValidElement(node)) {
+    if (inReact) {
+      componentsList.add(node.type);
+    }
+
+    const childrenAreReact = (inReact || node.type === AI.React) && !isJsxBoundary(node.type);
+    if ('children' in node.props) {
+      collectComponents(node.props.children, childrenAreReact, componentsList);
+    }
+  }
+
+  if (AI.isElement(node)) {
+    const childrenAreReact = (inReact || node.tag === AI.React) && !isJsxBoundary(node.tag);
+    if ('children' in node.props) {
+      collectComponents(node.props.children, childrenAreReact, componentsList);
+    }
+  }
+
+  return componentsList;
 }
 
 /**
@@ -40,149 +205,4 @@ function markLastElementIncomplete(
   Object.assign(serializedComponent, { possiblyIncomplete: true });
 
   markLastElementIncomplete(serializedComponent.children[serializedComponent.children.length - 1]);
-}
-
-export async function* UICompletion(
-  {
-    example,
-    aiComponents,
-    children,
-  }: { example: React.ReactNode; aiComponents?: AI.Component<any>[]; children: AI.Node },
-  { render, logger }: AI.ComponentContext
-) {
-  yield '';
-  const reactComponents = new Set<AI.Component<any> | React.JSXElementConstructor<any> | string>();
-
-  const validAIComponents = Object.fromEntries((aiComponents ?? []).map((c) => [reactComponentName(c), c]));
-  for (const component of aiComponents ?? []) {
-    reactComponents.add(component);
-  }
-
-  function collectComponents(node: React.ReactNode | AI.Node, inReact: boolean) {
-    if (Array.isArray(node)) {
-      node.forEach((node) => collectComponents(node, inReact));
-    }
-
-    if (React.isValidElement(node)) {
-      if (inReact) {
-        reactComponents.add(node.type);
-      }
-
-      const childrenAreReact = (inReact || node.type === AI.React) && !isJsxBoundary(node.type);
-      if ('children' in node.props) {
-        collectComponents(node.props.children, childrenAreReact);
-      }
-    }
-
-    if (AI.isElement(node)) {
-      const childrenAreReact = (inReact || node.tag === AI.React) && !isJsxBoundary(node.tag);
-      if ('children' in node.props) {
-        collectComponents(node.props.children, childrenAreReact);
-      }
-    }
-  }
-
-  collectComponents(example, true);
-
-  const validComponents = Object.fromEntries(Array.from(reactComponents).map((c) => [reactComponentName(c), c]));
-
-  const memoizedComponents = new Map<string, AI.Node>();
-
-  function getOrCreateMemo(serializedComponent: SerializedComponent, nonMemoized: AI.Node): AI.Node {
-    const hash = CryptoJS.SHA256(JSON.stringify(serializedComponent)).toString();
-    if (!memoizedComponents.has(hash)) {
-      memoizedComponents.set(hash, memo(nonMemoized));
-    }
-    return memoizedComponents.get(hash);
-  }
-
-  function toComponent(serializedComponent: SerializedComponent[] | SerializedComponent | string) {
-    if (Array.isArray(serializedComponent)) {
-      return <AI.Fragment>{serializedComponent.map(toComponent)}</AI.Fragment>;
-    }
-    if (!serializedComponent || typeof serializedComponent === 'string') {
-      return serializedComponent;
-    }
-
-    const Component = validComponents[serializedComponent.name];
-    if (!Component) {
-      logger.warn(
-        { serializedComponent },
-        `Ignoring component "${serializedComponent.name}" that wasn't present in the example. ` +
-          'You may need to adjust the prompt or include an example of this component.'
-      );
-      return null;
-    }
-
-    if (!('children' in serializedComponent)) {
-      throw new AIJSXError(
-        `JSON produced by the model did not fit the required schema: ${JSON.stringify(serializedComponent)}`,
-        ErrorCode.ModelOutputDidNotMatchUIShape,
-        'runtime'
-      );
-    }
-
-    // Sometimes the model returns a singleton string instead of an array.
-    const children =
-      typeof serializedComponent.children === 'string' ? [serializedComponent.children] : serializedComponent.children;
-
-    if (serializedComponent.name in validAIComponents) {
-      if (serializedComponent.possiblyIncomplete) {
-        return `Loading ${serializedComponent.name} ...`;
-      }
-      return (
-        <AI.jsx>{getOrCreateMemo(serializedComponent, <Component>{children.map(toComponent)}</Component>)}</AI.jsx>
-      );
-    }
-    return (
-      <AI.React>
-        <Component>{children.map(toComponent)}</Component>
-      </AI.React>
-    );
-  }
-
-  const Element: z.Schema = z.object({
-    name: z.string().refine((c) => Boolean(validComponents[c]), {
-      message: `Unknown component "name". Supported components: ${Object.keys(validComponents)}`,
-    }),
-    children: z.array(z.union([z.string(), z.lazy(() => Element)])),
-  });
-
-  const modelRenderGenerator = render(
-    <JsonChatCompletion
-      schema={Element}
-      retries={1}
-      example={'<SomeComponent /> becomes: { "name": "SomeComponent", "children": [] }'}
-    >
-      <SystemMessage>
-        You are an AI who is an expert UI designer. The user will provide content in the form of text and you will
-        respond using a set of React components to create a UI for the content. Here are the only available React
-        components with an example of how they should be used:
-        {'\n'}
-        ```tsx filename="example.tsx"{'\n'}
-        <AI.React>{example}</AI.React>
-        {'\n'}
-        ```
-        {'\n'}
-        Do not use any elements (including HTML elements) other than the ones above.
-        {'\n'}
-        Note that {'ImageGen'} is a special component that will generate an image for you. Its children should describe
-        the image you want to generate.
-      </SystemMessage>
-      <UserMessage>{children}</UserMessage>
-    </JsonChatCompletion>
-  )[Symbol.asyncIterator]();
-
-  while (true) {
-    const modelResult = await modelRenderGenerator.next();
-    const object = JSON.parse(modelResult.value);
-    if (!modelResult.done) {
-      markLastElementIncomplete(object);
-    }
-    const component = toComponent(object);
-    if (modelResult.done) {
-      return component;
-    }
-    yield component;
-  }
 }
