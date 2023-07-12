0e0f67e525544a4ccd6969e600f44f0e827214d7
Farzad Abdolhosseini <farzad@fixie.ai>
2023-07-12 16:41:59 -0700

    UICompletion interface: 1-shot + docs vs examples

--- packages/create-react-app-demo/src/story-teller/page.tsx
+++ packages/create-react-app-demo/src/story-teller/page.tsx
@@ -2,28 +2,26 @@
 import * as AI from 'ai-jsx/react';
 import { UICompletion } from 'ai-jsx/react/completion';
 import { useState, ReactNode } from 'react';
-import { ChatCompletion, UserMessage } from 'ai-jsx/core/completion';
 import { Prompt } from 'ai-jsx/batteries/prompts';
 import { ImageGen as BaseImageGen, ImageGenPropsWithChildren } from 'ai-jsx/core/image-gen';
 import ResultContainer from '../ResultContainer.tsx';
 import InputPrompt from '../InputPrompt.tsx';
 
-export function Header({ children }: { children: ReactNode }) {
+export function Header({ children }: { children?: ReactNode }) {
   return <h1 className="text-base font-semibold">{children}</h1>;
 }
-export function Paragraph({ children }: { children: ReactNode }) {
+export function Paragraph({ children }: { children?: ReactNode }) {
   return <p className="text-base">{children}</p>;
 }
 
-export function Container({ children }: { children: ReactNode }) {
+export function Container({ children }: { children?: ReactNode }) {
   return <div className="container">{children}</div>;
 }
 
 export function ImageGen(props: ImageGenPropsWithChildren) {
   return (
     <BaseImageGen size="256x256" {...props}>
-      A picture in the style of a children's book illustration:
-      {props.children}
+      A picture in the style of a children's book illustration: {props.children}
     </BaseImageGen>
   );
 }
@@ -42,28 +40,26 @@ export default function StoryTellerWrapper() {
       <ResultContainer title={`AI comes up with a story for "${query}"`}>
         <AI.jsx>
           <UICompletion
-            aiComponents={[ImageGen]}
-            example={
-              <Container>
-                <Header>Adventure in the Dark</Header>
-                <ImageGen>2 people in a dark forest. The lights of a cabin can be seen from afar.</ImageGen>
-                <Paragraph>Jimmy and Minny step out of the castle when all ....</Paragraph>
-                <Paragraph>They knock and a the door slides open with an alarming sound.</Paragraph>
-                <ImageGen>
-                  Creepy cabin in the woods at night, the door is slightly open but nothing since it's dark.
-                </ImageGen>
-                ...
-              </Container>
+            reactComponentsDoc={
+              <>
+                <Header />: A header component.
+                {'\n'}
+                <Paragraph />: A paragraph.
+                {'\n'}
+                <Container />: A container component. All other components must be enclosed with a top-level Container.
+              </>
+            }
+            aiComponentsDoc={
+              <>
+                <ImageGen />: A special component that will generate an image for you. All you need to do is to provide
+                a prompt that describes the image you want. The prompt should be descriptive enough to generate an image
+                that is relevant to the story.
+              </>
             }
           >
-            <ChatCompletion temperature={1}>
-              <Prompt persona="a fantasy fiction writer" />
-              <UserMessage>
-                Give me a story about {query}. Make sure to give it an interesting title. The story should have 3-5
-                chapters.
-              </UserMessage>
-            </ChatCompletion>
-            );
+            <Prompt persona="a fantasy fiction writer" />
+            Give me a story about {query}. Make sure to give it an interesting title. The story should have 3-5
+            chapters.
             {'\n'}
             Make sure to generate an image for each chapter to make it more interesting.
           </UICompletion>
