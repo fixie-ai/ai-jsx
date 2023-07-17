3ec9c326b4fa9f68a3ed9a4c49488bd0facc3475
Nick Heiner <nick.heiner+github@gmail.com>
2023-07-17 11:21:00 -0400

    Adding components

--- packages/nextjs-demo/src/app/blocks-jit-ui/page.tsx
+++ packages/nextjs-demo/src/app/blocks-jit-ui/page.tsx
@@ -1,9 +1,24 @@
-import { Paragraph } from "@/components/BuildingBlocks";
+import { Badge, Button, IconButton } from '@/components/BuildingBlocks';
+import { CheckCircleIcon, PlusIcon } from '@heroicons/react/20/solid';
 
 export default function RecipeExample() {
   return (
-    <>
-      <Paragraph>foo</Paragraph>
-    </>
+    <div className="gap-1.5 flex flex-col">
+      <div className="gap-1.5 flex">
+        <Button>
+          <CheckCircleIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" /> foo
+        </Button>
+        <Button primary>a b c foo</Button>
+        <Button>✅ foo</Button>
+        <IconButton>
+          <PlusIcon className="h-5 w-5" aria-hidden="true" />
+        </IconButton>
+      </div>
+      <div className="gap-1.5 flex">
+        <Badge color="red">foo</Badge>
+        <Badge color="green">foo</Badge>
+        <Badge color="indigo">foo</Badge>
+      </div>
+    </div>
   );
 }
--- packages/nextjs-demo/src/components/BuildingBlocks.tsx
+++ packages/nextjs-demo/src/components/BuildingBlocks.tsx
@@ -1,5 +1,51 @@
 import { ReactNode } from 'react';
+import classNames from 'classnames';
 
-export function Paragraph({ children }: { children: ReactNode }) {
-  return <p>{children}</p>
+export function Button({ children, primary }: { children: ReactNode, primary?: boolean }) {
+  return <button
+  type="button"
+  className={classNames("inline-flex items-center gap-x-1.5 rounded-md px-2.5 py-1.5 text-sm font-semibold shadow-sm", primary ? 'bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' : 'bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50')}
+>
+  {children}
+</button>
 }
+
+export function IconButton({ children }: { children: ReactNode }) {
+  return       <button
+  type="button"
+  className="rounded-full bg-indigo-600 p-1.5 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+>
+ {children} 
+</button>
+}
+
+type BackgroundColor = 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'indigo' | 'purple' | 'pink';
+export function Badge({ children, color }: { children: ReactNode, color: BackgroundColor }) {
+  function getColorClasses() {
+    switch (color) {
+      case 'red':
+        return 'bg-red-100 text-red-800 ring-red-600/20';
+      case 'green':
+        return 'bg-green-100 text-green-800 ring-green-600/20';
+      case 'yellow':
+        return 'bg-yellow-100 text-yellow-800 ring-yellow-600/20';
+      case 'blue':
+        return 'bg-blue-100 text-blue-800 ring-blue-600/20';
+      case 'indigo':
+        return 'bg-indigo-100 text-indigo-800 ring-indigo-600/20';
+      case 'purple':
+        return 'bg-purple-100 text-purple-800 ring-purple-600/20';
+      case 'pink':
+        return 'bg-pink-100 text-pink-800 ring-pink-600/20';
+      case 'gray':
+        return 'bg-gray-100 text-gray-800 ring-gray-600/20';
+      default:
+        throw new Error(`Unrecognized color: ${color}`)
+    }
+  }
+  return <span className={classNames('inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset', getColorClasses())}>
+  {children}
+</span>
+}
+
+// TODO: What's the best way to give the UI icons? Maybe stick to emojis for now?
