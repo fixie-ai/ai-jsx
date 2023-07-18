e2b2685a3a477973b12bf5ed51347e9a9d0a806c
Nick Heiner <nick.heiner+github@gmail.com>
2023-07-18 11:17:34 -0400

    Emitting what very well may be the right format

--- packages/examples/src/plugin.tsx
+++ packages/examples/src/plugin.tsx
@@ -1,85 +1,113 @@
-// import path from 'node:path'
-// import parser from '@babel/parser'
-// // @ts-expect-error
-// import estreeToBabel from 'estree-to-babel'
-// import {compileSync} from '@mdx-js/mdx'
+import { compile } from '@mdx-js/mdx';
+import { Jsonifiable } from 'type-fest';
+import _ from 'lodash';
 
-// export function babelPluginSyntaxMdx() {
-//   // Tell Babel to use a different parser.
-//   return {parserOverride: babelParserWithMdx}
-// }
-
-// // A Babel parser that parses MDX files with `@mdx-js/mdx` and passes any
-// // other things through to the normal Babel parser.
-// function babelParserWithMdx(value: any, options: any) {
-//   if (
-//     options.sourceFileName &&
-//     /\.mdx?$/.test(path.extname(options.sourceFileName))
-//   ) {
-//     // Babel does not support async parsers, unfortunately.
-//     return compileSync(
-//       {value, path: options.sourceFileName},
-//       // Tell `@mdx-js/mdx` to return a Babel tree instead of serialized JS.
-//       {recmaPlugins: [recmaBabel], /* jsxImportSource: …, otherOptions… */}
-//     ).result
-//   }
-
-//   return parser.parse(value, options)
-// }
-
-// // A “recma” plugin is a unified plugin that runs on the estree (used by
-// // `@mdx-js/mdx` and much of the JS ecosystem but not Babel).
-// // This plugin defines `'estree-to-babel'` as the compiler, which means that
-// // the resulting Babel tree is given back by `compileSync`.
-// function recmaBabel(this: any) {
-//   Object.assign(this, {Compiler: estreeToBabel})
-// }
-
-// // @ts-expect-error
-// import babel from '@babel/core'
-
-// // Note that a filename must be set for our plugin to know it’s MDX instead of JS.
-// console.log(JSON.stringify(await babel.parseAsync(`
-// import YouTube from "./YouTube";
-
-// # Welcome
-
-// <YouTube id="123" />
-// `, {filename: 'example.mdx', plugins: [babelPluginSyntaxMdx]}), null, 2))
-
-
-import {compile} from '@mdx-js/mdx'
-import {visit} from 'unist-util-visit'
-import {VFile} from 'vfile';
-
-function rehypePlugin(options: any) {
-  console.log('outer', options)
-  return (ast: any) => {
-    console.log('inner', JSON.stringify(ast, null, 2))
-    visit(ast, 'ParagraphNode', (node) => {
-      console.log(node)
-    })
-  }
+interface Node {
+  type: string;
+  tagName?: string;
+  children?: Node[];
+  name?: string;
+  value?: string;
+  attributes?: { type: string; name: string; value: string }[];
 }
 
-const file = new VFile({
-  path: '/Users/nth/code/ai-jsx/packages/examples/src/sample.mdx',
-  contents: `
-  import YouTube from "./YouTube";
+let ast: Node | undefined;
 
-  # Welcome
-  
-  <YouTube id="123" />
-  `});
+function rehypePlugin() {
+  return (_ast: Node) => {
+    ast = _ast;
+  };
+}
 
-console.log(await compile({
-  path: '/Users/nth/code/ai-jsx/packages/examples/src/sample.mdx',
-  value: `
-  import YouTube from "./YouTube";
+await compile(
+  {
+    path: '/Users/nth/code/ai-jsx/packages/examples/src/sample.mdx',
+    value: `
+import YouTube from "./YouTube";
 
-  # Welcome
-  
-  <YouTube id="123" />
-  `}, {
-  rehypePlugins: [[rehypePlugin, {throwOnError: true, strict: true}]]
-}))
+# Welcome
+
+<YouTube id="123" />
+
+<A d='asdf'><B c /></A>
+`,
+  },
+  {
+    rehypePlugins: [[rehypePlugin, { throwOnError: true, strict: true }]],
+  }
+);
+
+interface SerializedComponent {
+  $$type: 'element';
+  $$component: string;
+  props: Jsonifiable;
+}
+
+type Props = Jsonifiable & {
+  children?: Jsonifiable;
+};
+
+function convertAstToComponent(ast: Node): SerializedComponent | null {
+  const { type, tagName, children = [], name, attributes = [] } = ast;
+
+  let component = '';
+  let props: Props = {};
+
+  switch (type) {
+    case 'root': {
+      component = 'div';
+      // @ts-expect-error
+      props.children = _.compact(children.map(convertAstToComponent));
+      break;
+    }
+    case 'element': {
+      component = tagName!;
+      // @ts-expect-error
+      props.children = _.compact(children.map(convertAstToComponent));
+      break;
+    }
+    case 'mdxJsxFlowElement': {
+      component = name!;
+      // @ts-expect-error
+      props = attributes.reduce<Jsonifiable>((acc: any, attr: any) => {
+        if (attr.value?.type) {
+          // E.g. <A b={null} />
+          throw new Error(
+            `Unimplemented code path: handling a React component with a non-trivial prop ${JSON.stringify(
+              attr.value,
+              null,
+              2
+            )}`
+          );
+        }
+        acc[attr.name] = attr.value === null ? true : attr.value;
+        return acc;
+      }, {});
+      // @ts-expect-error
+      props.children = _.compact(children.map(convertAstToComponent));
+      break;
+    }
+    case 'text': {
+      if (ast.value === '\n') {
+        return { $$type: 'element', $$component: 'br', props: {} };
+      }
+      return { $$type: 'element', $$component: 'p', props: { children: ast.value } };
+    }
+    // We handle the imports separately.
+    case 'mdxjsEsm': {
+      return null;
+    }
+    default: {
+      throw new Error(`Unhandled type: ${JSON.stringify(ast, null, 2)}`);
+    }
+  }
+
+  return {
+    $$type: 'element',
+    $$component: component,
+    props,
+  };
+}
+
+const serializedComponent = convertAstToComponent(ast!);
+console.log(JSON.stringify(serializedComponent, null, 2));
