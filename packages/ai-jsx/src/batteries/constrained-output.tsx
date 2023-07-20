ed0ebcea9e9b44472de2835b06523cd6987c2774
Farzad Abdolhosseini <farzad.abdolhosseini@gmail.com>
2023-07-20 16:59:07 -0700

    Bugfix: schema and validators being ignored in JCCFC (#206)

--- packages/ai-jsx/src/batteries/constrained-output.tsx
+++ packages/ai-jsx/src/batteries/constrained-output.tsx
@@ -302,9 +302,12 @@ async function* ObjectCompletionWithRetry(
  * @hidden
  */
 export async function* JsonChatCompletionFunctionCall(
-  { schema, children, ...props }: ModelPropsWithChildren & { schema: z.Schema },
+  { schema, validators, children, ...props }: ObjectCompletion,
   { render }: AI.ComponentContext
 ) {
+  // If a schema is provided, it is added to the list of validators as well as the prompt.
+  const validatorsAndSchema = schema ? [schema.parse, ...(validators ?? [])] : validators ?? [];
+
   const childrenWithCompletion = (
     <ChatCompletion
       experimental_streamFunctionCallOnly
@@ -327,7 +330,26 @@ export async function* JsonChatCompletionFunctionCall(
 
   const frames = render(childrenWithCompletion);
   for await (const frame of frames) {
-    yield JSON.stringify(JSON.parse(frame).arguments);
+    const object = JSON.parse(frame).arguments;
+    try {
+      for (const validator of validatorsAndSchema) {
+        validator(object);
+      }
+    } catch (e: any) {
+      continue;
+    }
+    yield JSON.stringify(object);
   }
-  return JSON.stringify(JSON.parse(await frames).arguments);
+  const object = JSON.parse(await frames).arguments;
+  try {
+    for (const validator of validatorsAndSchema) {
+      validator(object);
+    }
+  } catch (e: any) {
+    throw new CompletionError('The model did not produce a valid JSON object', 'runtime', {
+      output: JSON.stringify(object),
+      validationError: e.message,
+    });
+  }
+  return JSON.stringify(object);
 }
