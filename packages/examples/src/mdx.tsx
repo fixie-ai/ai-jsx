087f14ba6003dd38136b943d7e5ba653f1acca29
Nick Heiner <nick.heiner+github@gmail.com>
2023-07-28 15:44:11 -0700

    Only emit parsable mdx

--- packages/examples/src/mdx.tsx
+++ packages/examples/src/mdx.tsx
@@ -1,14 +1,12 @@
 /** @jsxImportSource ai-jsx/react */
 import * as AI from 'ai-jsx';
 import { SystemMessage, UserMessage } from 'ai-jsx/core/completion';
-// import { showInspector } from 'ai-jsx/core/inspector';
+import { showInspector } from 'ai-jsx/core/inspector';
 import { MdxChatCompletion } from 'ai-jsx/react/jit-ui/mdx';
 import { JsonChatCompletion } from 'ai-jsx/batteries/constrained-output';
 import z from 'zod';
 
 import { OpenAI } from 'ai-jsx/lib/openai';
-import { PinoLogger } from 'ai-jsx/core/log';
-import { pino } from 'pino';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 function Card({ header, footer, children }: { header?: string; footer?: string; children: string }) {
@@ -115,7 +113,7 @@ function QuestionAndAnswer({ children }: { children: AI.Node }, { memo }: AI.Com
       <OpenAI chatModel="gpt-4">
         Q: {question}
         {'\n'}
-        A: <MdxChatCompletion usageExamples={usageExample}>{question}</MdxChatCompletion>
+        A: <MdxChatCompletion usageExamples={usageExample} alwaysParsable>{question}</MdxChatCompletion>
         {'\n\n'}
       </OpenAI>
     </>
@@ -166,20 +164,13 @@ export function App() {
 
 // showInspector(<App />);
 
-const logger = pino({
-  name: 'ai-jsx',
-  level: process.env.loglevel ?? 'trace',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-    },
-  },
-});
-
 let lastValue = '';
-const rendering = AI.createRenderContext({ logger: new PinoLogger(logger) }).render(<App />, { appendOnly: true });
+const rendering = AI.createRenderContext().render(<App />, { appendOnly: true });
 for await (const frame of rendering) {
+  // console.log(frame);
+  // console.log(frame.slice(lastValue.length));
   process.stdout.write(frame.slice(lastValue.length));
   lastValue = frame;
 }
+
+// console.log(await rendering);
