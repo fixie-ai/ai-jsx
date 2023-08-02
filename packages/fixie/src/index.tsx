9cafaf5e649c735c781fc74c814905b344d6e1c7
juberti <justin@fixie.ai>
2023-08-02 12:07:30 -0700

    cr

--- packages/fixie/src/index.tsx
+++ packages/fixie/src/index.tsx
@@ -1,15 +1,10 @@
-import { toTextStream } from 'ai-jsx/stream';
 import { ChatCompletion, SystemMessage, UserMessage } from 'ai-jsx/core/completion';
 
-function HelloWorld({ message }: { message: string }) {
+export default function HelloWorld({ message }: { message: string }) {
   return (
     <ChatCompletion>
       <SystemMessage>Respond to the user using some variant of the phrase "Hello World!".</SystemMessage>
       <UserMessage>{message}</UserMessage>
     </ChatCompletion>
   );
-}
-
-export default function handleMessage(message: string) {
-  return toTextStream(<HelloWorld message={message} />);
 }
