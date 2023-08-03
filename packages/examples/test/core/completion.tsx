c7c78927f83b1aef9d864302e6f90da1886cc77a
Nick Heiner <nick.heiner+github@gmail.com>
2023-08-03 17:19:06 -0400

    Attempt at nock

--- packages/examples/test/core/completion.tsx
+++ packages/examples/test/core/completion.tsx
@@ -1,15 +1,33 @@
+import nock from 'nock';
+import fetch from 'node-fetch';
+
+// @ts-expect-error
+globalThis.fetch = fetch;
+
 import * as AI from 'ai-jsx';
 import { ChatCompletion, UserMessage } from 'ai-jsx/core/completion';
 
-// jest.mock('openai', () => ({
-
-// }))
-
 it('passes all function fields', async () => {
+  const openaiScope = nock('https://api.openai.com')
+  .post('/v1/chat/completions', body => {
+    console.log('got body', body)
+    return true;
+  })
+  .reply(200, {
+    // Return a mocked response here
+    choices: [
+      {
+        text: 'Mocked response text',
+      },
+    ],
+  });
+
   const result = await AI.createRenderContext().render(
     <ChatCompletion>
       <UserMessage>Hello</UserMessage>
     </ChatCompletion>
   );
   expect(result).toEqual('fak');
+
+  openaiScope.done();
 });
