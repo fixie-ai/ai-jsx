1cb1ee3f67b9e7c1b4e9a75e44c46749b654bb63
Nick Heiner <nick.heiner+github@gmail.com>
2023-08-30 14:28:57 -0700

    Hacks to make it work locally

--- packages/sidekick-github/src/index.tsx
+++ packages/sidekick-github/src/index.tsx
@@ -3,7 +3,7 @@ import { ChatCompletion, SystemMessage, ConversationHistory } from 'ai-jsx/core/
 export default function HelloWorld() {
   return (
     <ChatCompletion temperature={1}>
-      <SystemMessage>Respond to the user as if you were Clippy from Microsoft Office.</SystemMessage>
+      <SystemMessage>Respond to the user as if you were a game of thrones character</SystemMessage>
     </ChatCompletion>
   );
 }
