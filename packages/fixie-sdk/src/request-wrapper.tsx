e972d1445d255f2ed4d05f2ec903cfb840281b2a
Peter Salas <peter@fixie.ai>
2023-08-31 14:43:55 -0700

    Wire up the OpenAI proxy

--- packages/fixie-sdk/src/request-wrapper.tsx
+++ packages/fixie-sdk/src/request-wrapper.tsx
@@ -3,6 +3,7 @@ import { InvokeAgentRequest } from './types.js';
 import { ShowConversation, ConversationHistoryContext } from 'ai-jsx/core/conversation';
 import { Json } from './json.js';
 import { FixieConversation } from './conversation.js';
+import { OpenAI, OpenAIClient, ValidChatModel } from 'ai-jsx/lib/openai';
 
 export interface FixieRequestContext {
   request: InvokeAgentRequest;
@@ -27,52 +28,70 @@ export function FixieRequestWrapper({
 }: FixieRequestContext & {
   children: AI.Node;
 }) {
+  let wrappedNode: AI.Node = (
+    <ShowConversation
+      present={(message) => {
+        switch (message.type) {
+          case 'user':
+          case 'assistant':
+            return [
+              <Json>
+                {{
+                  kind: 'text',
+                  content: message.element,
+                }}
+              </Json>,
+              '\n',
+            ];
+          case 'system':
+            return null;
+          case 'functionCall':
+            return [
+              <Json>
+                {{
+                  kind: 'functionCall',
+                  name: message.element.props.name,
+                  args: message.element.props.args,
+                }}
+              </Json>,
+              '\n',
+            ];
+          case 'functionResponse':
+            return [
+              <Json>
+                {{
+                  kind: 'functionResponse',
+                  name: message.element.props.name,
+                  response: <>{message.element.props.children}</>,
+                }}
+              </Json>,
+              '\n',
+            ];
+        }
+      }}
+    >
+      {children}
+    </ShowConversation>
+  );
+
+  // If we're using OpenAI (or default), enable the OpenAI proxy.
+  if (!request.generationParams.modelProvider || request.generationParams.modelProvider.toLowerCase() === 'openai') {
+    wrappedNode = (
+      <OpenAI
+        client={new OpenAIClient({ apiKey: authToken, baseURL: new URL('api/openai-proxy/v1', apiBaseUrl).toString() })}
+        chatModel={(request.generationParams.model as ValidChatModel) ?? 'gpt-3.5-turbo'}
+      >
+        {wrappedNode}
+      </OpenAI>
+    );
+  } else {
+    throw new Error(`The model provider ("${request.generationParams.modelProvider}") is not supported.`);
+  }
+
   return (
     <fixieContext.Provider value={{ request, agentId, authToken, apiBaseUrl }}>
       <ConversationHistoryContext.Provider value={<FixieConversation />}>
-        <ShowConversation
-          present={(message) => {
-            switch (message.type) {
-              case 'user':
-              case 'assistant':
-                return [
-                  <Json>
-                    {{
-                      kind: 'text',
-                      content: message.element,
-                    }}
-                  </Json>,
-                  '\n',
-                ];
-              case 'system':
-                return null;
-              case 'functionCall':
-                return [
-                  <Json>
-                    {{
-                      kind: 'functionCall',
-                      name: message.element.props.name,
-                      args: message.element.props.args,
-                    }}
-                  </Json>,
-                  '\n',
-                ];
-              case 'functionResponse':
-                return [
-                  <Json>
-                    {{
-                      kind: 'functionResponse',
-                      name: message.element.props.name,
-                      response: <>{message.element.props.children}</>,
-                    }}
-                  </Json>,
-                  '\n',
-                ];
-            }
-          }}
-        >
-          {children}
-        </ShowConversation>
+        {wrappedNode}
       </ConversationHistoryContext.Provider>
     </fixieContext.Provider>
   );
