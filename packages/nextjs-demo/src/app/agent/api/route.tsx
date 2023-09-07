02a1c2bd74be36651352a7c2930964d970ce374f
juberti <justin@fixie.ai>
2023-09-07 14:59:14 -0700

    agent v1

--- packages/nextjs-demo/src/app/agent/api/route.tsx
+++ packages/nextjs-demo/src/app/agent/api/route.tsx
@@ -1,44 +1,81 @@
 /** @jsxImportSource ai-jsx */
-import * as AI from "ai-jsx";
-import {
-  AssistantMessage,
-  ChatCompletion,
-  SystemMessage,
-  UserMessage,
-} from "ai-jsx/core/completion";
-import { NextRequest, NextResponse } from "next/server";
+import { AssistantMessage, ChatCompletion, SystemMessage, UserMessage } from 'ai-jsx/core/completion';
+import { StreamingTextResponse } from 'ai';
+import { toTextStream } from 'ai-jsx/stream';
+import { NextRequest } from 'next/server';
+
+const FOXIE_PROMPT = `
+You are a helpful and friendly fox. Always chat with the user in this
+persona, if the user hasn't sent a message yet, and tell them who you
+are. Only do this once. Keep your responses brief and conversational,
+and always try to keep the conversation going. Here are some facts about
+you: "Foxes are small to medium-sized, omnivorous mammals belonging to
+several genera of the family Canidae. They have a flattened skull,
+upright, triangular ears, a pointed, slightly upturned snout, and a long
+bushy tail ("brush"). Twelve species belong to the monophyletic "true
+fox" group of genus Vulpes. Approximately another 25 current or extinct
+species are always or sometimes called foxes; these foxes are either
+part of the paraphyletic group of the South American foxes, or of the
+outlying group, which consists of the bat-eared fox, gray fox, and
+island fox.[1] Foxes live on every continent except Antarctica. The most
+common and widespread species of fox is the red fox (Vulpes vulpes) with
+about 47 recognized subspecies.[2] The global distribution of foxes,
+together with their widespread reputation for cunning, has contributed
+to their prominence in popular culture and folklore in many societies
+around the world The hunting of foxes with packs of hounds, long an
+established pursuit in Europe, especially in the British Isles, was
+exported by European settlers to various parts of the New World."
+Remember, be concise!
+`;
+
+const KK_PROMPT = `
+You are a drive-thru order taker for Krispy Kreme. The set of available items is as shown below.
+Local time is currently: ${new Date().toLocaleTimeString()}
+Greet the user using a time-appropriate greeting based on their local time, e.g., "Good afternoon, what can I get for you today?",
+take their order, ask if there's anything else they would like to add, and finally,
+total up the order and ask the user to pull up to the drive thru window. 
+If the user only ordered a drink, ask them if they would like to add a donut to their order.
+If the user only ordered donuts, ask them if they would like to add a drink to their order.
+When speaking with the user, be concise, keep your responses to a sentence or two.
+
+# DONUTS
+
+PUMPKIN SPICE ORIGINAL GLAZED® DOUGHNUT $1.29
+PUMPKIN SPICE CAKE DOUGHNUT $1.29
+PUMPKIN SPICE CHEESECAKE SWIRL DOUGHNUT $1.29
+PUMPKIN SPICE MAPLE PECAN DOUGHNUT $1.29
+ORIGINAL GLAZED® DOUGHNUT $0.99
+CHOCOLATE ICED GLAZED DOUGHNUT $1.09
+CHOCOLATE ICED GLAZED DOUGHNUT WITH SPRINKLES $1.09
+GLAZED RASPBERRY FILLED DOUGHNUT $1.09
+GLAZED BLUEBERRY CAKE DOUGHNUT $1.09
+STRAWBERRY ICED DOUGHNUT WITH SPRINKLES $1.09
+GLAZED LEMON FILLED DOUGHNUT $1.09
+CHOCOLATE ICED CUSTARD FILLED DOUGHNUT $1.09
+CHOCOLATE ICED DOUGHNUT WITH KREME™ FILLING $1.09
+CAKE BATTER DOUGHNUT $1.09
+ORIGINAL GLAZED® DOUGHNUT HOLES $3.99
+
+# COFFEE & DRINKS 
+
+PUMPKIN SPICE COFFEE $2.59
+PUMPKIN SPICE LATTE $4.59
+CLASSIC BREWED COFFEE $1.79
+CLASSIC DECAF BREWED COFFEE $1.79
+LATTE $3.49
+VANILLA SPECIALTY LATTE $3.49
+ORIGINAL GLAZED® LATTE $3.49
+CARAMEL SPECIALTY LATTE $3.49
+CARAMEL MOCHA SPECIALTY LATTE $3.49
+MOCHA SPECIALTY LATTE $3.49
+`;
 
 function ChatAgent({ conversation }: { conversation: string[] }) {
   return (
     <ChatCompletion>
-      <SystemMessage>
-        You are a helpful and friendly fox. Always chat with the user in this
-        persona, if the user hasn't sent a message yet, and tell them who you
-        are. Only do this once. Keep your responses brief and conversational,
-        and always try to keep the conversation going. Here are some facts about
-        you: "Foxes are small to medium-sized, omnivorous mammals belonging to
-        several genera of the family Canidae. They have a flattened skull,
-        upright, triangular ears, a pointed, slightly upturned snout, and a long
-        bushy tail ("brush"). Twelve species belong to the monophyletic "true
-        fox" group of genus Vulpes. Approximately another 25 current or extinct
-        species are always or sometimes called foxes; these foxes are either
-        part of the paraphyletic group of the South American foxes, or of the
-        outlying group, which consists of the bat-eared fox, gray fox, and
-        island fox.[1] Foxes live on every continent except Antarctica. The most
-        common and widespread species of fox is the red fox (Vulpes vulpes) with
-        about 47 recognized subspecies.[2] The global distribution of foxes,
-        together with their widespread reputation for cunning, has contributed
-        to their prominence in popular culture and folklore in many societies
-        around the world The hunting of foxes with packs of hounds, long an
-        established pursuit in Europe, especially in the British Isles, was
-        exported by European settlers to various parts of the New World."
-      </SystemMessage>
+      <SystemMessage>{KK_PROMPT}</SystemMessage>
       {conversation.map((message, index) =>
-        index % 2 ? (
-          <AssistantMessage>{message}</AssistantMessage>
-        ) : (
-          <UserMessage>{message}</UserMessage>
-        ),
+        index % 2 ? <AssistantMessage>{message}</AssistantMessage> : <UserMessage>{message}</UserMessage>
       )}
     </ChatCompletion>
   );
@@ -47,8 +84,5 @@ function ChatAgent({ conversation }: { conversation: string[] }) {
 export async function POST(request: NextRequest) {
   const json = await request.json();
   console.log(`messages=${json.messages}`);
-  const outText = await AI.createRenderContext().render(
-    <ChatAgent conversation={json.messages} />,
-  );
-  return new NextResponse(JSON.stringify({ text: outText }));
+  return new StreamingTextResponse(toTextStream(<ChatAgent conversation={json.messages} />));
 }
--- packages/nextjs-demo/src/app/agent/page.tsx
+++ packages/nextjs-demo/src/app/agent/page.tsx
@@ -1,82 +1,174 @@
-"use client";
-import React from "react";
-import "../globals.css";
-import { MicManager, SpeechRecognitionFactory } from "../asr/asr";
-import { TextToSpeech } from "../tts/tts";
+'use client';
+import React, { useState } from 'react';
+import { DeepgramSpeechRecognition, MicManager, Transcript } from 'ai-jsx/lib/asr/asr';
+import { AzureTextToSpeech, TextToSpeechBase } from 'ai-jsx/lib/tts/tts';
+import '../globals.css';
 
-const manager: MicManager = new MicManager();
-let asr = null;
-const tts = new TextToSpeech("azure", "en-US-JennyNeural", 1.2);
-const messages = [];
+const pending: { [key: string]: AssistantRequest } = {};
+const messages: string[] = [];
 
-async function start(timeslice: number) {
-  if (!asr) {
-    asr = SpeechRecognitionFactory.create("deepgram", manager);
-    await manager.startMic(timeslice);
-    asr.addEventListener("transcript", (event) => {
-      if (event.detail.final) {
-        console.log(`input: ${event.detail.transcript}`);
-        handleInput(event.detail.transcript);
-      }
-    });
-    console.log("starting ASR");
-    asr.start();
-    requestOutput();
-  }
-}
+const ButtonComponent: React.FC<{ onClick: () => void; disabled: boolean; children: React.ReactNode }> = ({
+  onClick,
+  disabled,
+  children,
+}) => (
+  <button
+    onClick={onClick}
+    disabled={disabled}
+    className={`ml-2 rounded-md ${
+      disabled ? 'bg-gray-300' : 'bg-fixie-fresh-salmon hover:bg-fixie-ripe-salmon'
+    } px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-fixie-fresh-salmon`}
+  >
+    {children}
+  </button>
+);
 
-async function handleInput(text: string) {
-  messages.push(text);
-  requestOutput();
-}
-
-async function requestOutput() {
-  console.log("calling LLM");
-  const startTime = window.performance.now();
-  const res = await fetch("/agent/api", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ messages: messages }),
+/**
+ * Retrieves an ephemeral token from the server for use in a TTS service.
+ */
+async function GetToken(provider: string) {
+  const response = await fetch('/asr/api', {
+    method: 'POST',
+    body: JSON.stringify({ provider }),
   });
-  const data = await res.json();
-  console.log(
-    `output: ${data.text}, latency: ${Math.floor(
-      window.performance.now() - startTime,
-    )} ms`,
-  );
-  (document.getElementById("output") as HTMLTextAreaElement).value = data.text;
-  messages.push(data.text);
-  console.log("calling TTS");
-  tts.play(data.text);
+  const json = await response.json();
+  return json.token;
 }
 
-async function stop() {
-  tts.stop();
-  if (asr) {
-    console.log("stopping ASR");
-    await asr.close();
-    await manager.stop();
-    asr = null;
+/**
+ * Builds a URL for use in a TTS service.
+ */
+function BuildUrl(provider: string, voice: string, rate: number, text: string) {
+  return `/tts/api?provider=${provider}&voice=${voice}&rate=${rate}&text=${text}`;
+}
+
+class AssistantRequest {
+  public outMessage = '';
+  public done = false;
+  public onupdate?: (request: AssistantRequest, newText: string) => void;
+  public oncomplete?: (request: AssistantRequest) => void;
+  constructor(private readonly inMessages: string[], public active: boolean, public tts: TextToSpeechBase) {}
+
+  async start() {
+    console.log(`calling LLM for ${this.inMessages[this.inMessages.length - 1]}`);
+    const startTime = performance.now();
+    const res = await fetch('/agent/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ messages: this.inMessages }),
+    });
+    console.log(`Got LLM response, latency=${(performance.now() - startTime).toFixed(0)}`);
+    const reader = res.body!.getReader();
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) {
+        this.done = true;
+        this.oncomplete?.(this);
+        break;
+      }
+      if (!this.outMessage) {
+        console.log(`first token: ${this.outMessage} latency=${(performance.now() - startTime).toFixed(0)}`);
+      }
+      const newText = new TextDecoder().decode(value);
+      this.outMessage += newText;
+      this.onupdate?.(this, newText);
+    }
   }
 }
 
 const PageComponent: React.FC = () => {
-  const handleStart = () => start(100);
-  const handleStop = () => stop();
+  const [manager, setManager] = useState<MicManager | null>(null);
+  const [asr, setAsr] = useState<DeepgramSpeechRecognition | null>(null);
+  const [tts, setTts] = useState<AzureTextToSpeech | null>(null);
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+  const active = () => Boolean(manager);
+  const handleRequestUpdate = (request: AssistantRequest, newText: string) => {
+    console.log(`request update, active=${request.active}`);
+    if (request.active) {
+      setOutput(request.outMessage);
+      request.tts.play(newText);
+    }
+  };
+  const handleRequestDone = (request: AssistantRequest) => {
+    console.log(`request done, active=${request.active}`);
+    if (request.active) {
+      messages.push(request.outMessage);
+      setInput('');
+    } //++++ handle weird timing case where this happens before active
+  };
+  const handleInputUpdate = (text: string, final: boolean, tts: TextToSpeechBase) => {
+    const newMessages = [...messages, text];
+    const hit = text in pending;
+    console.log(`${final ? 'final' : 'partial'}: ${text} ${hit ? 'HIT' : 'MISS'}`);
+    if (!hit) {
+      const request = new AssistantRequest(newMessages, final, tts);
+      request.onupdate = handleRequestUpdate;
+      request.oncomplete = handleRequestDone;
+      pending[text] = request;
+      request.start();
+    } else {
+      const request = pending[text];
+      request.active = true;
+      setOutput(request.outMessage);
+      tts.play(request.outMessage);
+    }
+  };
+  const handleStop = () => {
+    if (!active()) {
+      return;
+    }
+    console.log('stopping');
+    tts?.stop();
+    asr?.close();
+    manager?.stop();
+    setTts(null);
+    setAsr(null);
+    setManager(null);
+  };
+  const handleStart = async () => {
+    const _manager = new MicManager();
+    const _asr = new DeepgramSpeechRecognition(_manager, GetToken);
+    // const _tts = new ElevenLabsTextToSpeech(ELEVEN_VOICE, 1.2);
+    const _tts = new AzureTextToSpeech(BuildUrl, AzureTextToSpeech.DEFAULT_VOICE, 1.4);
+    setManager(_manager);
+    setAsr(_asr);
+    setTts(_tts);
+    console.log('starting');
+    await _manager.startMic(100, () => handleStop());
+    _asr.addEventListener('transcript', (event: CustomEventInit<Transcript>) => {
+      const obj = event.detail!;
+      handleInputUpdate(obj.text, obj.final, _tts);
+      setInput(obj.text);
+    });
+    console.log('starting ASR');
+    _asr.start();
+    handleInputUpdate('Hi!', true, _tts);
+  };
+
   return (
     <>
-      <p className="font-sm ml-2 mb-2">
-        This demo allows you to chat (via voice) with a knowledgeable fox 🦊.
-        Click Start to begin.
-      </p>
-      <div className="font-bold">
-        <button onClick={handleStart}>Start</button>
-        <button onClick={handleStop}>Stop</button>
-      </div>
-      <div>
-        <textarea id="output" rows={10} cols={80}></textarea>
+      <div className="w-full">
+        <p className="font-sm ml-2 mb-2">
+          This demo allows you to chat (via voice) with a knowledgeable fox 🦊. Click Start to begin.
+        </p>
+        <div>
+          <textarea className="m-2 w-full" readOnly id="output" rows={10} cols={80} value={output}></textarea>
+        </div>
+        <div>
+          <input className="m-2 w-full text-lg" id="input" readOnly value={input}></input>
+        </div>
+        <div className="m-2 w-full flex justify-center">
+          <ButtonComponent disabled={active()} onClick={handleStart}>
+            Start Chatting
+          </ButtonComponent>
+          <ButtonComponent disabled={!active()} onClick={handleStop}>
+            Stop Chatting
+          </ButtonComponent>
+        </div>
       </div>
     </>
   );
