28b85341edc1e0b3a876f58887f01fe2f7bdb23c
Nick Heiner <nick.heiner+github@gmail.com>
2023-09-22 11:48:54 -0700

    Create Fixie embed component

--- packages/fixie/src/fixie-embed.tsx
+++ packages/fixie/src/fixie-embed.tsx
@@ -8,7 +8,14 @@ export interface FixieEmbedProps extends React.IframeHTMLAttributes<HTMLIFrameEl
   fixieHost?: string;
 }
 
-export function FixieEmbed({...iframeProps}: FixieEmbedProps) {
-  // @ts-expect-error
+export function FixieEmbed({speak, debug, agentId, fixieHost, ...iframeProps}: FixieEmbedProps) {
+  const embedUrl = new URL(`/embed/${agentId}`, fixieHost);
+  if (speak) {
+    embedUrl.searchParams.set('speak', '1');
+  }
+  if (debug) {
+    embedUrl.searchParams.set('debug', '1');
+  }
+
   return <iframe className='grow' src={embedUrl.toString()} allow='clipboard-write' {...iframeProps}></iframe>
 }
