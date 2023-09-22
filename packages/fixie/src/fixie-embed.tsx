6e2eeec333bd10a97b000bd794d530e32d62bb5a
Nick Heiner <nick.heiner+github@gmail.com>
2023-09-22 11:44:12 -0700

    Start of Fixie embed component

---
+++ packages/fixie/src/fixie-embed.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface FixieEmbedProps extends React.IframeHTMLAttributes<HTMLIFrameElement>{
+  agentId: string;
+  speak?: boolean;
+  debug?: boolean;
+
+  fixieHost?: string;
+}
+
+export function FixieEmbed({...iframeProps}: FixieEmbedProps) {
+  return <iframe className='grow' src={embedUrl.toString()} allow='clipboard-write' {...iframeProps}></iframe>
+}
