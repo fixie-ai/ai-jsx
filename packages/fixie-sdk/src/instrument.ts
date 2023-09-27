ac6e84c658ff4be4ec694fbc4dabbcaa5a8badfd
Peter Salas <peter@fixie.ai>
2023-09-26 22:01:49 -0700

    Propagate trace headers to FIXIE_API_URL (#348)
    When instrumenting `fetch` in `@fixieai/sdk`, ensure that tracing
    headers propagate back to the FIXIE_API_URL.

--- packages/fixie-sdk/src/instrument.ts
+++ packages/fixie-sdk/src/instrument.ts
@@ -6,13 +6,24 @@ import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-grpc';
 import { BatchLogRecordProcessor } from '@opentelemetry/sdk-logs';
 
+function escapeRegExp(value: string) {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
+  return value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+const fixieApiUrl = process.env.FIXIE_API_URL ?? 'https://api.fixie.ai';
+const fixieApiUrlWithSlash = fixieApiUrl.endsWith('/') ? fixieApiUrl : `${fixieApiUrl}/`;
+const fixieApiUrlRegex = new RegExp(`${escapeRegExp(fixieApiUrlWithSlash)}.*`);
+
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter(),
   logRecordProcessor: new BatchLogRecordProcessor(new OTLPLogExporter()),
   instrumentations: [
     new HttpInstrumentation(),
     new fastify_instrumentation.FastifyInstrumentation(),
-    new FetchInstrumentation(),
+    new FetchInstrumentation({
+      propagateTraceHeaderCorsUrls: [fixieApiUrlRegex],
+    }),
   ],
   serviceName: 'ai.jsx-fixie-agent',
 });
