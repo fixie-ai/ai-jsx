34dd2569b5a12c152a7a4528632c1974816e4b91
Nick Heiner <nick.heiner+github@gmail.com>
2023-10-06 19:40:33 -0400

    [Automated] Format

--- packages/fixie/web.ts
+++ packages/fixie/web.ts
@@ -1,3 +1,9 @@
 export { IsomorphicFixieClient } from './src/isomorphic-client.js';
-export { InlineFixieEmbed, FloatingFixieEmbed, ControlledFloatingFixieEmbed, getBaseIframeProps, FixieEmbedProps } from './src/fixie-embed.js';
+export {
+  InlineFixieEmbed,
+  FloatingFixieEmbed,
+  ControlledFloatingFixieEmbed,
+  getBaseIframeProps,
+  FixieEmbedProps,
+} from './src/fixie-embed.js';
 export * from './src/use-fixie.js';
