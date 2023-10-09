0c814f6d2bf98b84d99c4ae5c78e2fc6d7ff44ab
Zach Koch <zach@fixie.ai>
2023-10-09 09:25:30 -0700

    clean up existing demo

--- packages/voice/src/app/layout.tsx
+++ packages/voice/src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <main className="flex min-h-screen flex-col items-start px-24 py-6">{children}</main>
+        <main className="flex min-h-screen flex-col items-start px-4 lg:px-24 py-6">{children}</main>
       </body>
     </html>
   );
