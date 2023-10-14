0633fb5105ac4bdda7291f342c2480ce8ba84a33
juberti <justin@fixie.ai>
2023-10-13 17:37:50 -0700

    format

--- packages/voice/src/app/layout.tsx
+++ packages/voice/src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <main className="flex min-h-screen flex-col items-start px-4 lg:px-24 py-6">{children}</main>
-        <Analytics/>
+        <Analytics />
       </body>
     </html>
   );
