e3d5a431d5e0a6c7b1076781c2b53d7f4e2ef513
Peter Salas <peter@fixie.ai>
2023-10-24 11:42:39 -0700

    Remove FixieAgent export (#436)
    `agent.ts` contains CLI command implementation and shouldn't be coded
    against. (In particular, it can't be imported in web contexts at all.)

--- packages/fixie/index.ts
+++ packages/fixie/index.ts
@@ -1,4 +1,3 @@
-export { FixieAgent } from './src/agent.js';
 export { FixieClient } from './src/client.js';
 export { IsomorphicFixieClient } from './src/isomorphic-client.js';
 export * from './src/sidekick-types.js';
