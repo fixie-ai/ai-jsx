4fca90e475dfd137358fdcb954ee04393bcea974
Matt Welsh <matt@fixie.ai>
2023-10-26 23:01:30 -0700

    Add FixieAgent export.

--- packages/fixie/src/index.ts
+++ packages/fixie/src/index.ts
@@ -1,3 +1,4 @@
 export { FixieClient } from './client.js';
 export { IsomorphicFixieClient } from './isomorphic-client.js';
+export { FixieAgent } from './agent.js';
 export * from './sidekick-types.js';
